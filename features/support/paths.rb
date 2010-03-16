037f261a748a93cb5d9f142eab6df7679236795b
Andi <andi@0xb5.org>
2010-03-16 10:41:41 +0100

    updated cucumber

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -1,10 +1,9 @@
-
 module NavigationHelpers
   # Maps a name to a path. Used by the
   #
   #   When /^I go to (.+)$/ do |page_name|
   #
-  # step definition in webrat_steps.rb
+  # step definition in web_steps.rb
   #
   def path_to(page_name)
     case page_name
@@ -32,9 +31,8 @@ module NavigationHelpers
     when /the proposal/
       raise [@proposal.inspect,@proposal.parent.inspect].join('\n')
       question_proposal_path(@proposal.parent, @proposal)
-
     # Add more mappings here.
-    # Here is a more fancy example:
+    # Here is an example that pulls values out of the Regexp:
     #
     #   when /^(.*)'s profile page$/i
     #     user_profile_path(User.find_by_login($1))
@@ -45,7 +43,7 @@ module NavigationHelpers
         send(paths.join('_'))
       rescue
         raise "Can't find mapping from \"#{page_name}\" to a path.\n" +
-          "Now, go and add a mapping in #{__FILE__}"
+        "Now, go and add a mapping in #{__FILE__}"
       end
     end
   end
