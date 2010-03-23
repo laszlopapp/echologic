e7341a81a401fc2faa28d0f29fe4bd3c70e22a2c
Andi <andi@0xb5.org>
2010-03-23 12:23:58 +0100

    added show_profile to migration

--- db/migrate/20100322183701_calculate_completeness_for_existing_users.rb
+++ db/migrate/20100322183701_calculate_completeness_for_existing_users.rb
@@ -2,6 +2,7 @@ class CalculateCompletenessForExistingUsers < ActiveRecord::Migration
   def self.up
     Profile.all.each do |p|
       p.calculate_completeness
+      p.show_profile = 1
       p.save!
     end
   end
