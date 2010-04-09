ab92c42ec6cac358ff6920e0ff5a371df669e276
unknown <Tiago@.(none)>
2010-04-09 17:13:25 +0200

    changed the query from the controller to the model and changed the query creation

--- app/controllers/connect_controller.rb
+++ app/controllers/connect_controller.rb
@@ -11,11 +11,11 @@ class ConnectController < ApplicationController
     @value    = params[:value] || ""
     @sort     = params[:sort]  || ""
     @page     = params[:page]  || 1
-    @profiles = search(@sort, @value.split(' ').first)
+    @profiles = Profile.search_profiles(@sort, @value.split(' ').first)
 
     if @value.split(' ').size > 1
        for value in @value.split(' ')[1..-1] do
-        @profiles &= search(@sort, value)
+        @profiles &= Profile.search_profiles(@sort, value)
       end
     end
 
@@ -39,64 +39,6 @@ class ConnectController < ApplicationController
     end
   end
 
-  # Return connect page with results of the search
-  # FIXME: it's really sad that we can't use named scopes here
-  # that's why i had to add the 'show_profile = 1' condition directly to the searchlogic query
-  # method: POST
-  
-  def search (sort, value)
-    profiles = sort.blank? ? Profile : Profile.user_concernments_sort_equals(sort)        
-    #profiles = profiles.user_active_equals(1).show_profile_equals(1).first_name_or_last_name_or_city_or_country_or_about_me_or_motivation_or_user_email_or_user_tags_value_like_or_user_memberships_position_or_user_memberships_organisation_like(value).by_last_name_first_name_id
-    profiles = profiles.first_name_or_last_name_or_city_or_country_or_about_me_or_motivation_or_memberships_position_like(value)
-    
-  end
-
-  def search2(sort, value)
-
-    
-
-    sort_string = "c.sort = #{sort} AND " if !sort.blank?
-
-    query = <<-END
-      select distinct p.*, u.email
-      from
-        profiles p
-        LEFT JOIN users u        ON u.id = p.user_id
-        LEFT JOIN memberships m  ON u.id = m.user_id
-        LEFT JOIN concernments c ON (u.id = c.user_id)
-        LEFT JOIN tags t         ON (t.id = c.tag_id)
-      where
-    END
-    
-    query_cont = <<-END 
-        #{sort_string}
-        u.active = 1 AND
-        p.show_profile = 1 AND
-        (
-          p.first_name    like ?
-          or p.last_name  like ?
-          or p.city       like ?
-          or p.country    like ?
-          or p.about_me   like ?
-          or p.motivation like ?
-          or u.email      like ?
-          or t.value      like ?
-          or m.position   like ?
-          or m.organisation like ?
-        )
-        order by CASE WHEN p.last_name IS NULL OR p.last_name="" THEN 1 ELSE 0 END, p.last_name, p.first_name, u.id asc;
-    END
-    
-    #formatting the query string
-    query << sort_string unless sort.blank?
-    query << query_cont
-    value = "%#{value}%"    
-    query_array = [query, [value]*10].flatten
-    
-    #sql querying
-    profiles = Profile.find_by_sql(query_array)
-    
-  end
 
   # checks wether the users profile is complete enough to view other users profiles
   def check_completeness
