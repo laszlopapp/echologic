ae61a82ff92677fd7f101acbf766dae4cba8d4e6
Laszlo Papp <laszlo.papp@echologic.org>
2010-04-22 20:29:40 +0200

    Removed show_profile. Only profile_completeness counts. We sort accordingly and do not filter any more.

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,5 +1,8 @@
 class Profile < ActiveRecord::Base
 
+  # Constants
+  COMPLETENESS_THRESHOLD = 0.4
+
   # Every profile has to belong to a user.
   belongs_to :user,       :dependent => :destroy
   has_many :web_profiles, :through => :user
@@ -9,21 +12,15 @@ class Profile < ActiveRecord::Base
   validates_presence_of :user_id
   validates_length_of :about_me, :maximum => 1024, :allow_nil => true
   validates_length_of :motivation, :maximum => 1024, :allow_nil => true
-  
- 
+
+
   include ProfileExtension::Completeness
 
-
-  # named scope, only returning profiles with 'show_profile' flag set to true
-  # currently this flag is true for alle users before release 0.5 and everyone who ever had more then 50% of his profile filled
-  # FIXME: this scope isn't used, because the current profile search implementation doesn't work with additional scopes
-  named_scope :published, :conditions => { :show_profile => 1 }  
-  
   named_scope :by_last_name_first_name_id, :include => :user, :order => 'CASE WHEN last_name IS NULL OR last_name="" THEN 1 ELSE 0 END, last_name, first_name, user.id asc'
- 
+
   # callback for paperclip
- 
-  
+
+
   # There are two kind of people in the world..
   @@gender = {
     false => I18n.t('users.profile.gender.male'),
@@ -67,19 +64,19 @@ class Profile < ActiveRecord::Base
   end
 
   def self.search_profiles(sort, value, opts={} )
-    
+
     #sorting the or arguments
-    sort_string = "c.sort = #{sort}" 
+    sort_string = "c.sort = #{sort}"
     or_attrs = opts[:attrs] || %w(p.first_name p.last_name p.city p.country p.about_me p.motivation u.email t.value m.position m.organisation)
     or_conditions = or_attrs.map{|attr|"#{attr} LIKE ?"}.join(" OR ")
-    
+
     #sorting the and arguments
-    and_conditions = opts[:conditions] || ["u.active = 1","p.show_profile = 1"]
-    and_conditions.insert(0,sort_string) if !sort.blank?    
-    
+    and_conditions = opts[:conditions] || ["u.active = 1"]
+    and_conditions.insert(0,sort_string) if !sort.blank?
+
     #all getting along like really good friends
     and_conditions << "(#{or_conditions})"
-    
+
     #Rambo 1
     query_part_1 = <<-END
       select distinct p.*, u.email
@@ -94,12 +91,15 @@ class Profile < ActiveRecord::Base
     #Rambo 2
     query_part_2 = and_conditions.join(" AND ")
     #Rambo 3
-    query_part_3 = " order by CASE WHEN last_name IS NULL OR last_name='' THEN 1 ELSE 0 END, p.last_name, p.first_name, u.id asc;"
-    
+    query_part_3 = " order by
+      CASE WHEN p.completeness > #{COMPLETENESS_THRESHOLD} THEN 0 ELSE 1 END,
+      CASE WHEN p.last_name IS NULL OR p.last_name='' THEN 1 ELSE 0 END,
+      p.last_name, p.first_name, u.id asc;"
+
     #All Rambo's in one
     query = query_part_1+query_part_2+query_part_3
     value = "%#{value}%"
-     
+
     conditions = [query, *([value] * or_attrs.size)]
     profiles = find_by_sql(conditions)
   end
