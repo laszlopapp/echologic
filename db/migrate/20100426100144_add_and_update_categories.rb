509cb97e6290ca5d2d4b9a8d980b5bcf74bf4447
unknown <Tiago@.(none)>
2010-04-26 12:33:25 +0200

    add migration to update echonomy_jam category and add real_prices and echosocial

---
+++ db/migrate/20100426100144_add_and_update_categories.rb
@@ -0,0 +1,13 @@
+class AddAndUpdateCategories < ActiveRecord::Migration
+  def self.up
+      Tag.create!(:value => 'echosocial')
+      Tag.create!(:value => 'realprices')
+      Tag.find_by_value('echonomy_jam').update_attributes!({:value => 'echonomyjam'})
+  end
+
+  def self.down
+    Tag.find_by_value('echosocial').delete
+    Tag.find_by_value('realprices').delete
+    Tag.find_by_value('echonomyjam').update_attributes!({:value => 'echonomy_jam'})
+  end
+end
