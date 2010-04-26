f2b75e604e96c0b1072633ca2bc9f618b5bcbf03
unknown <Tiago@.(none)>
2010-04-26 17:11:02 +0200

    updated categories migration

--- db/migrate/20100426100144_add_and_update_categories.rb
+++ db/migrate/20100426100144_add_and_update_categories.rb
@@ -2,7 +2,8 @@ class AddAndUpdateCategories < ActiveRecord::Migration
   def self.up
     Tag.create!(:value => 'echosocial')
     Tag.create!(:value => 'realprices')
-    t = Tag.find_by_value('echonomyjam')
+    t = Tag.find_by_value('echonomyJAM')
+    t.value.replace('echonomyjamx')
     t.value.replace('echonomyjam')
     t.save
   end
@@ -11,6 +12,7 @@ class AddAndUpdateCategories < ActiveRecord::Migration
     Tag.find_by_value('echosocial').delete
     Tag.find_by_value('realprices').delete    
     t = Tag.find_by_value('echonomyjam')
+     t.value.replace('echonomyJAMx')
     t.value.replace('echonomyJAM')
     t.save
   end
