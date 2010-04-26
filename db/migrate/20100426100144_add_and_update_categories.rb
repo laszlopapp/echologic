7440a8949ecc817182d171ef0d1d198157058d59
unknown <Tiago@.(none)>
2010-04-26 18:20:21 +0200

    added text for real prices in english

--- db/migrate/20100426100144_add_and_update_categories.rb
+++ db/migrate/20100426100144_add_and_update_categories.rb
@@ -2,12 +2,19 @@ class AddAndUpdateCategories < ActiveRecord::Migration
   def self.up
     Tag.create!(:value => 'echosocial')
     Tag.create!(:value => 'realprices')
-    t = Tag.find_by_value('echonomyjam')
-    t.value = 'echonomyjamx'
-    t.save
-    t = Tag.find_by_value('echonomyjamx')
-    t.value = 'echonomyjam'
-    t.save
+#    t = Tag.find_by_value('echonomyJAM')
+##    t.value = 'echonomyjamx'
+##    t.save
+##    t = Tag.find_by_value('echonomyjamx')
+#    t.update_attributes!({:value => 'echonomyjam'})
+#    t.save
+
+    sql = ActiveRecord::Base.connection();
+    
+    sql.begin_db_transaction    
+    sql.update "UPDATE tags SET value='echonomyjam' WHERE value='echonomyJAM'";
+    sql.commit_db_transaction
+
   end
 
   def self.down
