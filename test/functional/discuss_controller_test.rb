f02cd0f3bbf1de8046b1fa39936eb38fc9cb77e3
Laszlo Papp <laszlo.papp@echologic.org>
2010-05-03 02:21:47 +0200

    Minor formatting.

--- test/functional/discuss_controller_test.rb
+++ test/functional/discuss_controller_test.rb
@@ -7,32 +7,32 @@ class DiscussControllerTest < ActionController::TestCase
     @controller = DiscussController.new
     @user = Profile.find_by_first_name('User')
   end
-  
+
   test "should get index without being logged on" do
     get :index
     assert_response :success
   end
-  
-  
+
+
   test "should get roadmap without being logged on" do
     get :roadmap
     assert_response :success
   end
-  
+
   test "should cancel question update and redirect to question page" do
     statement = Question.first
     session[:last_statement] = statement
     get :cancel
     assert_redirected_to question_url(statement)
   end
-  
+
   test "should cancel proposal update and redirect to proposal page" do
     statement = Proposal.first
     session[:last_statement] = statement
     get :cancel
     assert_redirected_to question_proposal_url(statement.parent, statement)
   end
-  
+
   test "should cancel improvement proposal update and redirect to improvement proposal page" do
     statement = ImprovementProposal.first
     session[:last_statement] = statement
