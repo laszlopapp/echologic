3c9512693a475360addad3f08ad221861dcb41d2
Laszlo Papp <laszlo.papp@echologic.org>
2010-05-03 02:26:41 +0200

    Minor (echoLogic -> echologic)

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,10 +2,10 @@
 
 # Your secret key for verifying cookie session data integrity.
 # If you change this key, all old sessions will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
+# Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 ActionController::Base.session = {
-  :key         => '_echoLogic_session',
+  :key         => '_echologic_session',
   :secret      => 'e130611999f809cd7b0d4b66320fa8e58e66c438f5a528b506c5dc7d323c408cb81d000616342ddc10ae722e5075127e7024c29c2010b44391d0ca81e39d314a'
 }
 
