8e0dc4571f74fa03e01c2c0cb96256640b39da35
Laszlo Papp <laszlo.papp@echologic.org>
2010-05-13 19:35:40 +0200

    Fixed 99-er Web_Address.

--- db/migrate/20100506111607_rename_column_sort_to_web_address_id_from_web_address.rb
+++ db/migrate/20100506111607_rename_column_sort_to_web_address_id_from_web_address.rb
@@ -3,7 +3,7 @@ class RenameColumnSortToWebAddressIdFromWebAddress < ActiveRecord::Migration
     add_column :web_addresses, :web_address_type_id, :integer
     WebAddress.all.each do |web_address|
       web_address_type = web_address.sort.to_i
-      web_address_type = web_address_type != 99 ? web_address_type+1 : 99
+      web_address_type += 1 unless web_address_type == 99
       web_address.web_address_type_id = EnumKey.find_by_key_and_name(web_address_type, "web_address_types").id
       web_address.save(false)
     end
