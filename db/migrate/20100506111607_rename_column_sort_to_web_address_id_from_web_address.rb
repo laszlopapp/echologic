3f00daa984a4bc1131d9d6183b5be338831734de
unknown <Tiago@cardoso@echologic.org>
2010-05-14 20:15:27 +0200

    added a new migration

--- db/migrate/20100506111607_rename_column_sort_to_web_address_id_from_web_address.rb
+++ db/migrate/20100506111607_rename_column_sort_to_web_address_id_from_web_address.rb
@@ -2,20 +2,11 @@ class RenameColumnSortToWebAddressIdFromWebAddress < ActiveRecord::Migration
   def self.up
     rename_column :web_addresses, :location, :address
     add_column :web_addresses, :web_address_type_id, :integer
-    WebAddress.all.each do |web_address|
-      key = (web_address.sort == 99 ? web_address.sort : web_address.sort + 1)
-      puts key.to_s
-      web_address.web_address_type_id = EnumKey.find_by_key_and_enum_name(key, "web_address_types").id
-      web_address.save(false)
-    end
-    
-    #remove_column :web_addresses, :sort
-#    rename_column :web_addresses, :sort, :web_address_type_id
     
   end
 
   def self.down
     rename_column :web_addresses, :address, :location
-    rename_column :web_addresses, :web_address_type_id, :sort
+    remove_column :web_addresses, :web_address_type_id
   end
 end
