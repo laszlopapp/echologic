5773d613cd592272c82784e2300bd856eb7abdea
unknown <Tiago@cardoso@echologic.org>
2010-05-14 11:07:59 +0200

    added some debug printf methods on a migration

--- db/migrate/20100506111607_rename_column_sort_to_web_address_id_from_web_address.rb
+++ db/migrate/20100506111607_rename_column_sort_to_web_address_id_from_web_address.rb
@@ -3,9 +3,12 @@ class RenameColumnSortToWebAddressIdFromWebAddress < ActiveRecord::Migration
     add_column :web_addresses, :web_address_type_id, :integer
     WebAddress.all.each do |web_address|
       web_address_type = web_address.sort.to_i
+      puts "former #{web_address_type}"
       web_address_type += 1 unless web_address_type == 99
+      puts "Enum Key: #{EnumKey.find_by_key_and_name(web_address_type, "web_address_types").inspect}"
       web_address.web_address_type_id = EnumKey.find_by_key_and_name(web_address_type, "web_address_types").id
       web_address.save(false)
+      puts "later #{web_address.web_address_id.to_s}"
     end
     remove_column :web_addresses, :sort
   end
