eb8994df6d6d6802b17288d240965d09f9f41a0c
unknown <Tiago@cardoso@echologic.org>
2010-05-14 19:57:42 +0200

    corrected some migrations

--- db/migrate/20100506111607_rename_column_sort_to_web_address_id_from_web_address.rb
+++ db/migrate/20100506111607_rename_column_sort_to_web_address_id_from_web_address.rb
@@ -1,23 +1,14 @@
 class RenameColumnSortToWebAddressIdFromWebAddress < ActiveRecord::Migration
   def self.up
-    
-    add_column :web_addresses, :web_address_type_id, :integer
-    WebAddress.all.each do |old_web_address|
-      puts WebAddress.inspect
-      new_web_address = WebAddress.new
-      new_web_address.user_id = old_web_address.user_id      
-      new_web_address.location = old_web_address.location
-      key = (old_web_address.sort == 99 ? old_web_address.sort : old_web_address.sort + 1)
-      puts key.to_s
-      new_web_address.web_address_type = EnumKey.find_by_key_and_enum_name(key, "web_address_types")
-      
-      puts new_web_address.web_address_type.code
-     
-      new_web_address.save(false)
-      
-      old_web_address.destroy
-    end
     rename_column :web_addresses, :location, :address
+    add_column :web_addresses, :web_address_type_id, :integer
+    WebAddress.all.each do |web_address|
+      key = (web_address.sort == 99 ? web_address.sort : web_address.sort + 1)
+      puts key.to_s
+      web_address.web_address_type_id = EnumKey.find_by_key_and_enum_name(key, "web_address_types").id
+      web_address.save(false)
+    end
+    
     #remove_column :web_addresses, :sort
 #    rename_column :web_addresses, :sort, :web_address_type_id
     
