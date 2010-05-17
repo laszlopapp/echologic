d6886f5de4ed3d0b0d64e08f341a91e951f99c1f
unknown <Tiago@cardoso@echologic.org>
2010-05-17 10:58:29 +0200

    changed some migration

--- db/migrate/20100514181357_change_web_address_type.rb
+++ db/migrate/20100514181357_change_web_address_type.rb
@@ -2,13 +2,13 @@ class ChangeWebAddressType < ActiveRecord::Migration
   def self.up
     WebAddress.all.each do |web_address|
       key = (web_address.sort == 99 ? web_address.sort : web_address.sort + 1)
-      puts key.to_s
       web_address.web_address_type = EnumKey.find_by_key_and_enum_name(key, "web_address_types")
       web_address.save(false)
-      puts web_address.web_address_type_id.to_s
     end
+    
   end
 
   def self.down
+    
   end
 end
