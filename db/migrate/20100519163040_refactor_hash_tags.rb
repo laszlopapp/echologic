aff7573edcfa587a3bff8bf170929076f220a27e
unknown <Tiago@cardoso@echologic.org>
2010-05-19 19:43:25 +0200

    changed main tags to hash tags (with a # before them)

---
+++ db/migrate/20100519163040_refactor_hash_tags.rb
@@ -0,0 +1,18 @@
+class RefactorHashTags < ActiveRecord::Migration
+  def self.up
+    %w(echonomyjam echo echocracy echosocial realprices).each do |name|
+      tag = Tag.find_by_value(name)
+      tag.value= "##{tag.value}"
+      tag.save
+      puts tag.value
+    end
+  end
+
+  def self.down
+    %w(echonomyjam echo echocracy echosocial realprices).each do |name|
+      tag = Tag.find_by_value("##{value}")
+      tag.value = value
+      tag.save!
+    end
+  end
+end
