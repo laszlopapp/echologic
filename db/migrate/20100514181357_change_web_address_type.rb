feac230753776f92ba453d57c9ef19dbff8f6485
unknown <Tiago@cardoso@echologic.org>
2010-05-21 10:32:02 +0200

    added a rake task for the WebAddress sort to type_id migration

--- db/migrate/20100514181357_change_web_address_type.rb
+++ db/migrate/20100514181357_change_web_address_type.rb
@@ -1,10 +1,6 @@
 class ChangeWebAddressType < ActiveRecord::Migration
   def self.up
-    WebAddress.all.each do |web_address|
-      key = (web_address.sort == 99 ? web_address.sort : web_address.sort + 1)
-      web_address.type_id = EnumKey.find_by_key_and_enum_name(key, "web_address_types").id
-      web_address.save(false)
-    end
+    
     
   end
 
