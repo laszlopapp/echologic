c245e870ab653a11fc555c05e53c7ff57695773d
niklas <niklas@brueckenschlaeger.de>
2010-06-02 16:37:01 +0200

    always set Content-Length when retrieving LT from CAS (for nginx compatibility)

--- app/helpers/cas_helper.rb
+++ app/helpers/cas_helper.rb
@@ -2,9 +2,11 @@ module CasHelper
   def cas_login_token
     base_uri = URI.parse(CASClient::Frameworks::Rails::Filter.config[:cas_base_url])
     use_ssl = (base_uri.scheme == 'https' ? true : false)
-    http = Net::HTTP.new(base_uri.host, use_ssl ? 443 : 80)
+    http = Net::HTTP.new(base_uri.host, base_uri.port)
     http.use_ssl = use_ssl
     req = Net::HTTP::Post.new('/loginTicket')
+    # nginx fails with 411 if POST requests have no Content-Length set
+    req['Content-Length'] = 0
     return http.request(req).body
   end
 end
