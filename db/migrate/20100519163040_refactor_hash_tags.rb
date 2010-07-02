972b0514c5c2bfb3e0c0ea0b0cef544dbb5d5dd9
Laszlo Papp <laszlo.papp@echologic.org>
2010-07-02 14:58:42 +0200

    Seeding after tag conversion.

--- db/migrate/20100519163040_refactor_hash_tags.rb
+++ db/migrate/20100519163040_refactor_hash_tags.rb
@@ -1,13 +1,15 @@
 class RefactorHashTags < ActiveRecord::Migration
   def self.up
-    # Seeding new data into the DB
-    Rake::Task['db:seed'].invoke
 
+    # Updating featured topic tags to be #tags
     %w(echonomyjam echo echocracy echosocial realprices igf klimaherbsttv).each do |name|
       tag = Tag.find_by_value(name)
       tag.value= "##{tag.value}"
       tag.save
     end
+
+    # Seeding new data into the DB
+    Rake::Task['db:seed'].invoke
   end
 
   def self.down
