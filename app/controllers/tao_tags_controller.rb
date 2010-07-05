efafa6da4814ee46c5c8cdbe9de868f5cce8e353
Laszlo Papp <laszlo.papp@echologic.org>
2010-07-05 18:13:32 +0200

    Adjusted to work good with context specific tags.

--- app/controllers/tao_tags_controller.rb
+++ app/controllers/tao_tags_controller.rb
@@ -21,30 +21,33 @@ class TaoTagsController < ApplicationController
   #
   def create
     previous_completeness = current_user.profile.percent_completed
-    tags = params[:tag][:value].split(',')
-    new_tags = tags - current_user.tao_tags.map{|tao_tag|tao_tag.tag.value}
-    current_user.add_tags(tags,{:language_id => locale_language_id, :context_id => params[:context_id]})
-    context_code = EnumKey.find(params[:context_id]).code
-    current_user.profile.calculate_completeness #maybe there's a better solution for this...
+    tags = params[:tag][:value].split(',').map!{|t|t.strip}
+    context = EnumKey.find(params[:context_id])
+    new_tags = tags - current_user.tao_tags.in_context(context).map{|tao_tag|tao_tag.tag.value}
+    current_user.add_tags(new_tags, {:language_id => locale_language_id, :context_id => params[:context_id]})
+    current_user.profile.calculate_completeness
+
     respond_to do |format|
       format.js do
         if current_user.save
           current_completeness = current_user.profile.percent_completed
-          if previous_completeness != current_completeness do
+          if previous_completeness != current_completeness
             set_info("discuss.messages.new_percentage", :percentage => current_completeness)
           end
-          tags_to_show = current_user.tao_tags.select{|tao_tag| new_tags.include? tao_tag.tag.value}
+          tags_to_show = current_user.tao_tags.in_context(context).select do |tao_tag|
+            new_tags.include? tao_tag.tag.value
+          end
           render_with_info do |p|
-            p.insert_html :bottom, "tao_tags_#{context_code}",
+            p.insert_html :bottom, "tao_tags_#{context.code}",
                           :partial => "tao_tags/tao_tag",
                           :collection => tags_to_show
             p.visual_effect :appear, dom_id(tags_to_show.last) unless tags_to_show.empty?
-            p << "$('#new_tag_#{context_code}').reset();"
-            p << "$('#tag_#{context_code}_id').focus();"
-         end
-         else
+            p << "$('#new_tag_#{context.code}').reset();"
+            p << "$('#tag_#{context.code}_id').focus();"
+          end
+        else
           show_error_messages(current_user)
-         end
+        end
       end
     end
   end
@@ -59,19 +62,19 @@ class TaoTagsController < ApplicationController
     @tao_tag = TaoTag.find(params[:id])
     previous_completeness = current_user.profile.percent_completed
     @tao_tag.destroy
-    current_completeness = @tao_tag.tao.profile.nil? ? previous_completeness : @tao_tag.tao.profile.percent_completed
-    set_info("discuss.messages.new_percentage", :percentage => current_completeness) if previous_completeness != current_completeness
+    current_user.profile.calculate_completeness
+    current_completeness = current_user.profile.percent_completed
+    if previous_completeness != current_completeness
+      set_info("discuss.messages.new_percentage", :percentage => current_completeness)
+    end
 
     respond_to do |format|
       format.js do
-        # sorry, but this was crap. you can't add additional js actions like this...
-        # either use a rjs, a js, or a render :update block
-        # remove_container("concernment_#{params[:id]}")
         render_with_info do |p|
           p.remove dom_id(@tao_tag)
         end
-        #render :template => 'users/profile/remove_object', :locals => { :object => @concernment }
       end
     end
   end
+
 end
