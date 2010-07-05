e2199b983d10cddd5d76e456de554242b540e5dc
Laszlo Papp <laszlo.papp@echologic.org>
2010-07-05 12:13:19 +0200

    Formating.

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,28 +3,37 @@ class Tag < ActiveRecord::Base
 
   enum :language, :enum_name => :languages
 
-  ### ASSOCIATIONS:
-
+  # ASSOCIATIONS
   has_many :tao_tags, :dependent => :destroy
 
-  ### VALIDATIONS:
-
+  # VALIDATIONS
   validates_presence_of :value
   validates_uniqueness_of :value, :scope => :language_id
 
-  ### NAMED SCOPES:
-
+  # NAMED SCOPES
   def self.uber (*value)
     value.each {|d| puts d.class }
   end
 
-  named_scope :named, lambda { |language_id, value| { :conditions => ["value LIKE ? AND language_id = ?", value, language_id] } }
-  named_scope :named_any, lambda { |language_id, *list| { :conditions => list.map { |tag| sanitize_sql(["value LIKE ?", tag.to_s]) }.join(" OR ").concat(sanitize_sql([" AND language_id = ?", language_id])) } }
-  named_scope :named_like, lambda { |language_id, value| { :conditions => ["value LIKE ? AND language_id = ?", "%#{value}%", language_id] } }
-  named_scope :named_like_any, lambda { |language_id, *list| { :conditions => list.map { |tag| sanitize_sql(["value LIKE ?", "%#{tag.to_s}%"]) }.join(" OR ").concat(sanitize_sql([" AND language_id = ?", language_id])) } }
+  named_scope :named, lambda { |language_id, value|
+    { :conditions => ["value LIKE ? AND language_id = ?", value, language_id] }
+  }
+  named_scope :named_any, lambda { |language_id, *list|
+    { :conditions => list.map { |tag|
+      sanitize_sql(["value LIKE ?", tag.to_s]) }.join(" OR ").concat(sanitize_sql([" AND language_id = ?", language_id]))
+    }
+  }
+  named_scope :named_like, lambda { |language_id, value|
+    { :conditions => ["value LIKE ? AND language_id = ?", "%#{value}%", language_id] }
+  }
+  named_scope :named_like_any, lambda { |language_id, *list|
+    { :conditions => list.map { |tag|
+      sanitize_sql(["value LIKE ?", "%#{tag.to_s}%"]) }.join(" OR ").
+        concat(sanitize_sql([" AND language_id = ?", language_id]))
+    }
+  }
 
-  ### CLASS METHODS:
-
+  # CLASS METHODS
   def self.find_or_create_with_named_by_value(value, language_id = self.languages.first.id)
     named(language_id, value).first || Tag.create(:value => value, :language_id => language_id)
   end
@@ -41,14 +50,17 @@ class Tag < ActiveRecord::Base
     language_id = list.last.kind_of?(Numeric) ? list.pop : self.languages.first.id
 
     existing_tags = Tag.named_any(language_id, list).all
-    new_tag_values = list.reject { |value| existing_tags.any? { |tag| tag.value.mb_chars.downcase == value.mb_chars.downcase } }
-    created_tags  = new_tag_values.map { |value| Tag.create(:value => value, :language_id => language_id) }
+    new_tag_values = list.reject { |value|
+      existing_tags.any? { |tag| tag.value.mb_chars.downcase == value.mb_chars.downcase }
+    }
+    created_tags  = new_tag_values.map { |value|
+      Tag.create(:value => value, :language_id => language_id)
+    }
 
     existing_tags + created_tags
   end
 
-  ### INSTANCE METHODS:
-
+  # INSTANCE METHODS
   def ==(object)
     super || (object.is_a?(Tag) && value == object.value && language_id == object.language_id)
   end
