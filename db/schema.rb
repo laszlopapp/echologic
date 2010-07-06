35798aa729324e7a3fbb4201cd3402e17b83268c
Laszlo Papp <laszlo.papp@echologic.org>
2010-07-06 11:05:07 +0200

    Removed category_id from StatementNodes.

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
-# This file is auto-generated from the current state of the database. Instead of editing this file,
+# This file is auto-generated from the current state of the database. Instead of editing this file, 
 # please use the migrations feature of Active Record to incrementally modify your database, and
 # then regenerate this schema definition.
 #
@@ -130,7 +130,6 @@ ActiveRecord::Schema.define(:version => 20100705090233) do
     t.integer  "document_id"
     t.integer  "creator_id"
     t.integer  "echo_id"
-    t.integer  "category_id"
     t.integer  "state_id"
     t.datetime "created_at"
     t.datetime "updated_at"
