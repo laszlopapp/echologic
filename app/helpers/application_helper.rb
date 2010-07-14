c623984a5b473a109b1e6c742070c77d315534f1
Laszlo Papp <laszlo.papp@echologic.org>
2010-07-14 11:52:15 +0200

    Factored out save and cancel button block.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,6 +7,18 @@ module ApplicationHelper
     allow :admin
   end
 
+  def save_or_cancel(cancel_action)
+    val = submit_tag I18n.t('application.general.save'),
+                     :class => 'text_button save_button'
+    val << "<span class='or_button'>"
+    val << I18n.t('application.general.or')
+    val << "</span>"
+    val << link_to(I18n.t('application.general.cancel'),
+                   cancel_action,
+                  :class => 'ajax text_button cancel_text_button')
+    val
+  end
+
   # Return a progressbar
   def insert_progressbar(percent)
     tooltip = I18n.t('application.roadmap.progress_tooltip', :progress => percent)
