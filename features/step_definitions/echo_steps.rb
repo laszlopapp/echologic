d16890f4f20c44f0effad9d03084754965236375
Laszlo Papp <laszlo.papp@echologic.org>
2010-07-20 17:25:27 +0200

    Added comprehensive echo test.

--- features/step_definitions/echo_steps.rb
+++ features/step_definitions/echo_steps.rb
@@ -3,10 +3,30 @@ Given /^a proposal without echos$/ do
   @proposal.user_echos.destroy_all
 end
 
+Given /^I have the "([^\"]*)" proposal$/ do |title|
+  @proposal = StatementNode.search_statement_nodes(:type => "Proposal",
+                                                   :search_term => title,
+                                                   :language_ids => [EnumKey.find_by_code("en")]).first
+end
+
+
 Then /^the proposal should have one echo$/ do
   @proposal.reload
   i = UserEcho.count(:conditions => ["echo_id = ? and supported = 1", @proposal.echo.id])
   i >= 1
+end
+
+Then /^the proposal should have ([^\"]*) supporters$/ do |supporter_count|
+  @proposal.reload
+  user_echos = UserEcho.count(:conditions => ["echo_id = ? and supported = 1", @proposal.echo.id])
+  assert supporter_count.to_i == user_echos
+  assert supporter_count.to_i == @proposal.echo.supporter_count
+end
+
+Then /^the proposal should have ([^\"]*) visitors$/ do |visitor_count|
+  user_echos = UserEcho.count(:conditions => ["echo_id = ? and visited = 1", @proposal.echo.id])
+  assert visitor_count.to_i == user_echos
+  assert visitor_count.to_i == @proposal.echo.visitor_count
 end
 
 Then /^the proposal should have one visitor but no echos$/ do
@@ -36,4 +56,28 @@ end
 Then /^the proposal should not have "([^\"]*)" as follower$/ do |name|
   @proposal.reload
   assert !@user.follows?(@proposal)
+end
+
+Then /^I am supporter of the proposal$/ do
+  @proposal.reload
+  assert @proposal.supported?(@user)
+end
+
+Then /^I am not supporter of the proposal$/ do
+  @proposal.reload
+  assert !@proposal.supported?(@user)
+end
+
+Then /^the proposal should have "([^\"]*)" as supporters$/ do |users|
+  @proposal.reload
+  users.split(",").each do |user_name|
+    assert @proposal.supported?(Profile.find_by_first_name(user_name.strip).user)
+  end
+end
+
+Then /^the proposal should have "([^\"]*)" as visitors$/ do |users|
+  @proposal.reload
+  users.split(",").each do |user_name|
+    assert @proposal.visited?(Profile.find_by_first_name(user_name.strip).user)
+  end
 end
