6ec9075a6dd7328d8ea0687e3bb51d1b1a13ca35
unknown <Tiago@cardoso@echologic.org>
2010-07-27 20:30:11 +0200

    added uncommitted file

---
+++ config/initializers/mail_notification.rb
@@ -0,0 +1,3 @@
+ActivityTrackingNotification.quoficient = 1#7
+ActivityTrackingNotification.tracking_period = 7.days.ago
+ActivityTrackingNotification.delivery_period = 6.hours.from_now#Time.now.tomorrow.midnight
