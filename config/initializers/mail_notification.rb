447d48e3a6c200427d4a284e51a87628a3d1bb1e
Laszlo Papp <laszlo.papp@echologic.org>
2010-07-30 16:15:58 +0200

    Fixed syntax.

--- config/initializers/mail_notification.rb
+++ config/initializers/mail_notification.rb
@@ -1,3 +1,3 @@
 ActivityTrackingNotification.user_split_condition = {}#{:conditions => ["(id % 7) = ?", week_day]}
 ActivityTrackingNotification.tracking_period = 10.minutes.ago #7.days.ago
-ActivityTrackingNotification.delivery_period = Time.10.minutes.from_now #6.hours.from_now #Time.now.tomorrow.midnight
+ActivityTrackingNotification.delivery_period = 10.minutes.from_now #6.hours.from_now #Time.now.tomorrow.midnight
