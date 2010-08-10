7b389119093a631b6c9841335d9b192fc66bef3e
Laszlo Papp <laszlo.papp@echologic.org>
2010-08-10 18:16:01 +0200

    Adjusted migration name.

--- db/migrate/20100727103529_add_drafting_state_to_statement_node.rb
+++ db/migrate/20100727103529_add_drafting_state_to_statement_node.rb
@@ -1,4 +1,4 @@
-class AddDraftingStateToStatementNodeTable < ActiveRecord::Migration
+class AddDraftingStateToStatementNode < ActiveRecord::Migration
   def self.up
     add_column :statement_nodes, :drafting_state, :string, :limit => 20
   end
