28eb747dc5ec5d495e8cc0ce7a4db228d9cd28f5
Laszlo Papp <laszlo.papp@echologic.org>
2010-08-10 18:15:07 +0200

    Added require.

--- config/initializers/mail_notification.rb
+++ config/initializers/mail_notification.rb
@@ -1,3 +1,5 @@
+require 'activity_notification_service/activity_tracking_notification'
+
 ActivityTrackingNotification.user_split_condition = {}#{:conditions => ["(id % 7) = ?", week_day]}
 ActivityTrackingNotification.tracking_period = 10.minutes.ago #7.days.ago
 ActivityTrackingNotification.delivery_period = 10.minutes.from_now #6.hours.from_now #Time.now.tomorrow.midnight
