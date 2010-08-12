9133c53931736e286a4f7409a9dc53b691157b38
unknown <Tiago@cardoso@echologic.org>
2010-08-12 19:15:02 +0200

    added migration

---
+++ db/migrate/20100812170217_add_locked_by_and_locked_at_to_statement_document.rb
@@ -0,0 +1,10 @@
+class AddLockedByAndLockedAtToStatementDocument < ActiveRecord::Migration
+  def self.up
+    add_column :statement_documents, :locked_by, :integer
+    add_column :statement_documents, :locked_at, :integer
+  end
+
+  def self.down
+    remove_columns :statement_documents, :locked_by, :locked_at
+  end
+end
