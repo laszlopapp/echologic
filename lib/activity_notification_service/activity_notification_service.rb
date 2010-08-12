6acaaf8ec911a7af9f01041312ec28991c7a8d83
Laszlo Papp <laszlo.papp@echologic.org>
2010-08-12 16:48:33 +0200

    ...

--- lib/activity_notification_service/activity_notification_service.rb
+++ lib/activity_notification_service/activity_notification_service.rb
@@ -3,13 +3,13 @@ require 'singleton'
 
 class ActivityNotificationService
   include Singleton
-  
-  attr_accessor :period, :charges, :counter 
-  
+
+  attr_accessor :period, :charges, :counter
+
   def initialize
     @counter = 0
   end
-  
+
   def update(*args)
     send(*args)
   end
@@ -17,18 +17,18 @@ class ActivityNotificationService
   def supported(echoable, user)
     echoable.add_subscriber(user)
   end
-  
+
   def unsupported(echoable, user)
     echoable.remove_subscriber(user)
   end
-  
+
   def incorporated(echoable, user)
     statement_document = echoable.original_document
-    supporters = echoable.parent.supporters.select{|sup|sup.languages('advanced').include?(echoable.original_language)}
+    supporters = echoable.parent.supporters
     email = ActivityTrackingMailer.create_incorporation_notification(echoable, statement_document, supporters)
     ActivityTrackingMailer.deliver(email)
   end
-  
+
   def enqueue_activity_tracking_job
     Delayed::Job.enqueue ActivityTrackingJob.new(@counter%@charges, @charges, @period), 0, @period/@charges.from_now
   end
