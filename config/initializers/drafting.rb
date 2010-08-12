216ca48d98b65ac461985ac33b66cec427e08461
Laszlo Papp <laszlo.papp@echologic.org>
2010-08-12 16:44:30 +0200

    Adjsuted delay times.

--- config/initializers/drafting.rb
+++ config/initializers/drafting.rb
@@ -10,7 +10,7 @@ ActiveRecord::Base.send :include, ActiveRecord::Acts::Incorporable
 ActiveRecord::Base.send :include, ActiveRecord::Acts::Drafteable
 
 DraftingService.min_quorum = 50
-DraftingService.min_votes  = 5
-DraftingService.time_ready  = (60 * 60 * 10) # 10 hours
-DraftingService.time_approved  = (60 * 60 * 10) # 10 hours
-DraftingService.time_approval_reminder  = (60 * 60 * 6) # 6 hours
+DraftingService.min_votes  = 3
+DraftingService.time_ready  = 24.hours
+DraftingService.time_approved  = 24.hours
+DraftingService.time_approval_reminder  = 12.hours
