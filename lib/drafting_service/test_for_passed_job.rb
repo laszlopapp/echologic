ed78f7af9654aa5b520027b81e28b1b1793af599
Laszlo Papp <laszlo.papp@echologic.org>
2010-08-16 08:46:43 +0200

    Translation.

--- lib/drafting_service/test_for_passed_job.rb
+++ lib/drafting_service/test_for_passed_job.rb
@@ -1,19 +1,25 @@
 class TestForPassedJob < Struct.new(:incorporable_id)
 
   def perform
-    incorporable = StatementNode.find(incorporable_id)
-    if !incorporable.nil? and !incorporable.incorporated?
-      incorporable.times_passed += 1
-      incorporable.drafting_info.save
-      incorporable.reload
-      if incorporable.times_passed == 1
-        DraftingService.instance.send_passed_email(incorporable)
-        DraftingService.instance.stage(incorporable)
-      elsif incorporable.times_passed == 2
-        DraftingService.instance.send_supporters_passed_email(incorporable)
-        DraftingService.instance.reset_incorporable(incorporable)
-        DraftingService.instance.select_approved(incorporable)
+    begin
+      StatementNode.transation do
+        incorporable = StatementNode.find(incorporable_id)
+        if !incorporable.nil? and !incorporable.incorporated?
+          incorporable.times_passed += 1
+          incorporable.drafting_info.save
+          incorporable.reload
+          if incorporable.times_passed == 1
+            DraftingService.instance.stage(incorporable)
+            DraftingService.instance.send_passed_email(incorporable)
+          elsif incorporable.times_passed == 2
+            DraftingService.instance.reset_incorporable(incorporable)
+            DraftingService.instance.select_approved(incorporable)
+            DraftingService.instance.send_supporters_passed_email(incorporable)
+          end
+        end
       end
+    rescue StandardError => error
+      logger.error error.backtrace
     end
   end
 end
