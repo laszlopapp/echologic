56070a6ab041c4f75871b3b9e7c4f15204dcfccf
Laszlo Papp <laszlo.papp@echologic.org>
2010-08-16 09:32:44 +0200

    Rails default logger + adjusted action order.

--- lib/drafting_service/test_for_passed_job.rb
+++ lib/drafting_service/test_for_passed_job.rb
@@ -9,17 +9,17 @@ class TestForPassedJob < Struct.new(:incorporable_id)
           incorporable.drafting_info.save
           incorporable.reload
           if incorporable.times_passed == 1
-            DraftingService.instance.stage(incorporable)
             DraftingService.instance.send_passed_email(incorporable)
+            DraftingService.instance.stage(incorporable)
           elsif incorporable.times_passed == 2
+            DraftingService.instance.send_supporters_passed_email(incorporable)
             DraftingService.instance.reset_incorporable(incorporable)
             DraftingService.instance.select_approved(incorporable)
-            DraftingService.instance.send_supporters_passed_email(incorporable)
           end
         end
       end
     rescue StandardError => error
-      logger.error error.backtrace
+      RAILS_DEFAULT_LOGGER.error error.backtrace
     end
   end
 end
