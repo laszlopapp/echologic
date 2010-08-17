c9ef88d381391b0d6b250cc442d55c62ad556657
Laszlo Papp <laszlo.papp@echologic.org>
2010-08-17 19:05:19 +0200

    Discuss 20% ready.

--- app/models/development.rb
+++ app/models/development.rb
@@ -1,7 +1,7 @@
 class Development
 
-  #Roadmap progress constants
-  DISCUSS_PROGRESS = 15
+  # Roadmap progress constants
+  DISCUSS_PROGRESS = 20
   CONNECT_PROGRESS = 5
   ACT_PROGRESS = 0
   MY_ECHO_PROGRESS = 10
