1cde4742b0d5a832f8d3bd281cef2c688bfd4b3f
Laszlo Papp <laszlo.papp@echologic.org>
2010-08-17 19:07:47 +0200

    Job logic moved to DraftingService.

--- lib/drafting_service/approval_reminder_mail_job.rb
+++ lib/drafting_service/approval_reminder_mail_job.rb
@@ -3,11 +3,7 @@ class ApprovalReminderMailJob < Struct.new(:incorporable_id, :timestamp)
   def perform
     incorporable = StatementNode.find(incorporable_id)
     if !incorporable.nil? and incorporable.approved? and incorporable.state_since == timestamp
-      if incorporable.times_passed == 0
-        DraftingService.instance.send_approval_reminder(incorporable)
-      elsif incorporable.times_passed == 1
-        DraftingService.instance.send_supporters_approval_reminder(incorporable)
-      end
+      DraftingService.instance.remind(incorporable)
     end
   end
 end
--- lib/drafting_service/test_for_passed_job.rb
+++ lib/drafting_service/test_for_passed_job.rb
@@ -1,33 +1,9 @@
 class TestForPassedJob < Struct.new(:incorporable_id)
 
   def perform
-    begin
-      StatementNode.transaction do
-        incorporable = StatementNode.find(incorporable_id)
-        if !incorporable.nil? and !incorporable.incorporated?
-          incorporable.times_passed += 1
-          incorporable.drafting_info.save
-          incorporable.reload
-
-          # First round
-          if incorporable.times_passed == 1
-            DraftingService.instance.send_passed_email(incorporable)
-            DraftingService.instance.stage(incorporable)
-
-          # Second round
-          elsif incorporable.times_passed == 2
-            DraftingService.instance.send_supporters_passed_email(incorporable)
-            DraftingService.instance.reset_incorporable(incorporable)
-            DraftingService.instance.select_approved(incorporable)
-          end
-        end
-      end
-
-    rescue StandardError => error
-      Rails.logger.error "Error occured while executing test for passed job for IP '#{incorporable_id}':\n" +
-        error.backtrace
-    else
-      Rails.logger.info "Test for passed job has completed successfully."
+    incorporable = StatementNode.find(incorporable_id)
+    if !incorporable.nil? and !incorporable.incorporated?
+      DraftingService.instance.pass(incorporable)
     end
   end
 end
