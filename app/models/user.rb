9b287f7ab8217c902c6ba949ee2a1a733bdca89d
Laszlo Papp <laszlo.papp@echologic.org>
2010-08-18 16:20:04 +0200

    Factored out Activity Tracking logic (and removed perishable from it - has nothing to do with it!)

--- app/models/user.rb
+++ app/models/user.rb
@@ -100,16 +100,7 @@ class User < ActiveRecord::Base
     RegistrationMailer.deliver(mail)
   end
 
-  #Send an activity tracking email through mailer
-  def deliver_activity_tracking_email!(question_events, question_tags, events)
-    reset_perishable_token!
-    mail = ActivityTrackingMailer.create_activity_tracking_email(self, question_events, question_tags, events)
-    ActivityTrackingMailer.deliver(mail)
-  end
-
-
-  handle_asynchronously :deliver_activity_tracking_email!
-
+  
   ##
   ## PERMISSIONS
   ##
