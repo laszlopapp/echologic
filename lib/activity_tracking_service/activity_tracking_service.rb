f6ae700dbb9d606f5c838ec8beafbc11685b8141
Laszlo Papp <laszlo.papp@echologic.org>
2010-08-18 16:23:54 +0200

    Commented.

--- lib/activity_tracking_service/activity_tracking_service.rb
+++ lib/activity_tracking_service/activity_tracking_service.rb
@@ -1,10 +1,10 @@
 require 'singleton'
 
-
 class ActivityTrackingService
   include Singleton
 
   attr_accessor :period, :charges, :counter
+  #handle_asynchronously :send_activity_tracking_email
 
   def initialize
     @counter = -1
@@ -39,22 +39,26 @@ class ActivityTrackingService
   # Sends an activity tracking E-Mail to the given recipient.
   #
   def send_activity_tracking_email(recipient, question_events, question_tags, events)
-    mail = ActivityTrackingMailer.create_activity_tracking_email(recipient, question_events, question_tags, events)
+    mail = ActivityTrackingMailer.create_activity_tracking_email(recipient,
+                                                                 question_events,
+                                                                 question_tags,
+                                                                 events)
     ActivityTrackingMailer.deliver(mail)
   end
-
-  #handle_asynchronously :send_activity_tracking_email
-
 
   #
   # Manages the counter to calculate current charge and schedules the next job with it.
   #
   def enqueue_next_activity_tracking_job(current_charge = 0)
-    if @counter == -1
-      @counter = current_charge
-    end
+
+    # After restarting the process (on new deployment) the
+    # counter is initialized with the first running charge.
+    @counter = current_charge if @counter == -1
+
+    # Enqueuing the next job
     @counter += 1
     Delayed::Job.enqueue ActivityTrackingJob.new(@counter%@charges, @charges, @period), 0,
                          current_time.advance(:seconds => @period/@charges)
   end
+  
 end
