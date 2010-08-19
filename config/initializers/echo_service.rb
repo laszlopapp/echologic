00d99106a49f982b5f8343ae6ecff959f3463c2c
Laszlo Papp <laszlo.papp@echologic.org>
2010-08-19 04:33:16 +0200

    Renamed and changed dependencies between initializers for services.

--- config/initializers/echo_service.rb
+++
@@ -1,6 +0,0 @@
-require 'echo_service/echo_service'
-require 'activity_tracking_service/activity_tracking_service'
-
-#observers
-EchoService.instance.add_observer(DraftingService.instance)
-#EchoService.instance.add_observer(ActivityTrackingService.instance)
