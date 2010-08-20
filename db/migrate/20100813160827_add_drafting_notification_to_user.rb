25cf4ad4db7f82d90e2676066a8688e09acbb765
Laszlo Papp <laszlo.papp@echologic.org>
2010-08-20 03:04:06 +0200

    Drafting notification will not be initialized here (with value 1) but in a separate rake task.

--- db/migrate/20100813160827_add_drafting_notification_to_user.rb
+++ db/migrate/20100813160827_add_drafting_notification_to_user.rb
@@ -1,10 +1,6 @@
 class AddDraftingNotificationToUser < ActiveRecord::Migration
   def self.up
     add_column :users, :drafting_notification, :integer
-    User.all.each do |user|
-      user.drafting_notification = 1
-      user.save
-    end
   end
 
   def self.down
