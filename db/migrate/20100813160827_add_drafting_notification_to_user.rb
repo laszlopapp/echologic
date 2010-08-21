dee2895cd340cc11e2357e927ea3c15cfc345825
Laszlo Papp <laszlo.papp@echologic.org>
2010-08-21 16:57:49 +0200

    Initialization is done be rake tasks.

--- db/migrate/20100813160827_add_drafting_notification_to_user.rb
+++ db/migrate/20100813160827_add_drafting_notification_to_user.rb
@@ -1,10 +1,6 @@
 class AddDraftingNotificationToUser < ActiveRecord::Migration
   def self.up
     add_column :users, :drafting_notification, :integer, :default => 1
-    User.all.each do |user|
-      user.drafting_notification = 0
-      user.save
-    end
   end
 
   def self.down
