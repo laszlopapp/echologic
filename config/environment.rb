a4865503220f83cdc39c65ffa859d4b249f33fe7
Laszlo Papp <laszlo.papp@echologic.org>
2010-08-22 01:38:01 +0200

    Set UTF-8 as default charset for ActionMailer.

--- config/environment.rb
+++ config/environment.rb
@@ -49,8 +49,6 @@ Rails::Initializer.run do |config|
   # gem for running the background jobs in production
   config.gem 'daemons'
 
-
-
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
@@ -65,6 +63,9 @@ Rails::Initializer.run do |config|
   # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
   # Run "rake -D time" for a list of tasks for finding time zone names.
   config.time_zone = 'UTC'
+
+  # Action Mailer
+  config.action_mailer.default_charset = "utf-8"
 
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
