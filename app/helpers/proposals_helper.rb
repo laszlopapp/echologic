cb9c55b22f63fcf7091091088272a21d9415bad1
Laszlo Papp <laszlo.papp@echologic.org>
2010-08-22 06:04:14 +0200

    Fixed how to show incorporate button in not logged in state.

--- app/helpers/proposals_helper.rb
+++ app/helpers/proposals_helper.rb
@@ -5,11 +5,12 @@ module ProposalsHelper
   end
 
   def incorporate_statement_node_link(parent_node, parent_document, statement_node, statement_document)
-    if statement_node.published? and
-      parent_document.language == statement_node.drafting_language and
-      statement_document.language. == statement_node.drafting_language and
-      ((statement_node.times_passed == 0 and statement_document.author == current_user) or
-       (statement_node.times_passed == 1 and statement_node.supported?(current_user)))
+    if !current_user or 
+       (statement_node.published? and
+        parent_document.language == statement_node.drafting_language and
+        statement_document.language. == statement_node.drafting_language and
+        ((statement_node.times_passed == 0 and statement_document.author == current_user) or
+         (statement_node.times_passed == 1 and statement_node.supported?(current_user))))
 
       link_to(incorporate_proposal_path(statement_node),
              :id => 'incorporate_link',
