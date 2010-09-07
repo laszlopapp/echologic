76aa5ed78de5d88a2b134b1472ba47dcf579c097
unknown <Tiago@cardoso@echologic.org>
2010-09-07 19:49:01 +0200

    corrected old migration (redundant, only local significance)

--- db/migrate/20100705090233_update_tags_language_id.rb
+++ db/migrate/20100705090233_update_tags_language_id.rb
@@ -1,7 +1,7 @@
 class UpdateTagsLanguageId < ActiveRecord::Migration
   def self.up
     Tag.all.each do |tag|
-      tag.language = Tag.languages("de").first if tag.language.nil?
+      tag.language = Tag.languages("de") if tag.language.nil?
       tag.save
     end
   end
