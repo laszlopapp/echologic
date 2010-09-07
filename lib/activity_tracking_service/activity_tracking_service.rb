ab0cce5c10600406c4c6844fd117bd6a6c9a7c55
unknown <Tiago@cardoso@echologic.org>
2010-09-07 19:50:33 +0200

    subscription logic now directly implemented in the service created listener new event generation adaptation of the new logic to the email sending process

--- lib/activity_tracking_service/activity_tracking_service.rb
+++ lib/activity_tracking_service/activity_tracking_service.rb
@@ -23,17 +23,31 @@ class ActivityTrackingService
     subscription = echoable.subscriptions.find_by_subscriber_id(user.id) ||
                      Subscription.new(:subscriber => user, :subscriber_type => user.class.name,
                                       :subscribeable => echoable, :subscribeable_type => echoable.class.name)
-    subscriptions << subscription if subscription.new_record?
+    echoable.subscriptions << subscription if subscription.new_record?
+    # When echoable is a proposal, then we must follow the main discussion too
+    if !echoable.parent.nil?
+      parent_subscription = echoable.parent.subscriptions.find_by_subscriber_id(user.id) ||
+                       Subscription.new(:subscriber => user, :subscriber_type => user.class.name,
+                                        :subscribeable => echoable.parent, :subscribeable_type => echoable.parent.class.name)
+      user.subscriptions << parent_subscription if parent_subscription.new_record?
+    end
   end
 
   def unsupported(echoable, user)
     return if user.nil?
     subscription = echoable.subscriptions.find_by_subscriber_id(user.id)
-    subscriptions.delete(subscription) if subscription
+    echoable.subscriptions.delete(subscription) if subscription
+    # When a proposal, then we must remove the discussion subscription in the case when no more sibling is around
+    if !echoable.parent.nil?
+      parent_subscription = user.subscriptions.find_by_subscribeable_id(echoable.parent_id)
+      if (user.subscriptions.map(&:subscribeable_id) & echoable.parent.children_statements.map(&:id)).empty?
+        user.subscriptions.delete(parent_subscription) if parent_subscription
+      end
+    end
   end
   
-  def created(subscribeable)
-    created_event(subscribeable)
+  def created(node)
+    created_event(node)
   end
 
   def incorporated(echoable, user)
@@ -47,29 +61,24 @@ class ActivityTrackingService
   #
   # Creates an event for the newly created subscribeable object
   #
-  def create_event(subscribeable)
-    event_json = subscribeable.to_json(:include => {
-                                :statement => {
-                                  :include =>  {
-                                    :statement_documents => {
-                                      :only => [:title, :language_id]
-                                    }
-                                  }
-                                },
-                                :tao_tags => {
-                                  :include => {
-                                    :tag => {
-                                      :only => :value
-                                    }
-                                  }
-                                }
-                              },
-                              :only => [:root_id,:parent_id,:type])
+  def created_event(node)
+    
+    event_json = {
+      :type => node.class.name.underscore.downcase,
+      :id => node.id,
+      :tags => node.topic_tags,
+      :documents => set_titles_hash(node.statement_documents),
+      :parent_documents => node.parent ? set_titles_hash(node.parent.statement_documents) : nil,
+      :root_documents => (node.root and node.root != node.parent) ? set_titles_hash(node.root.statement_documents) : nil,
+      :parent_id => node.parent_id || -1,
+      :root_id => (!node.root_id.nil? and node.root_id != node.parent_id) ? node.root_id : -1,
+      :parent_type => node.parent ? node.parent.class.name.underscore.downcase : nil
+    }.to_json
 
-    events << Event.new(:event => event_json,
-                      :subscribeable => subscribeable,
-                      :subscribeable_type => subscribeable.class.name,
-                      :operation => 'new')
+    node.events << Event.new(:event => event_json,
+                             :operation => node.parent.nil? ? "new" : "new_child",
+                             :subscribeable_type => node.class.name,
+                             :subscribeable => node.parent)
   end
 
   #
@@ -104,29 +113,21 @@ class ActivityTrackingService
 
       # Collecting events
       events = Event.find_tracked_events(recipient, after_time)
-      puts "events: #{events.size}"
       next if events.blank? #if there are no events to send per email, take the next user
 
-      question_events = events.select{|e|JSON.parse(e.event).keys[0] == 'question'}
-      tags = Hash.new
+      question_events = events.select{|e|JSON.parse(e.event)['type'] == 'question'}
+      tag_counts = Hash.new
       question_events.each do |question|
         question_data = JSON.parse(question.event)
-        question_data['question']['tao_tags'].each do |tao_tag|
-          tags[tao_tag['tag']['value']] = tags[tao_tag['tag']['value']] ? tags[tao_tag['tag']['value']] + 1 : 1
-        end
+        question_data['tags'].each{|tag| tag_counts[tag] = tag_counts[tag] ? tag_counts[tag] + 1 : 1 }
       end
       events.sort! do |a,b|
-        a_parsed = JSON.parse(a.event)
-        root_x = a_parsed[a_parsed.keys[0]]['root_id'] || -1
-        parent_x = a_parsed[a_parsed.keys[0]]['parent_id'] || -1
-        b_parsed = JSON.parse(b.event)
-        root_y = b_parsed[b_parsed.keys[0]]['root_id'] || -1
-        parent_y = b_parsed[b_parsed.keys[0]]['parent_id'] || -1
-        [root_x,parent_x] <=> [root_y,parent_y]
+        a_parsed = JSON.parse(a.event) ; b_parsed = JSON.parse(b.event)
+        [a_parsed['root_id'],a_parsed['parent_id']] <=> [b_parsed['root_id'],b_parsed['parent_id']]
       end
 
       # Sending the mail
-      send_activity_email(recipient, question_events, tags, events - question_events)
+      send_activity_email(recipient, question_events, tag_counts, events - question_events)
     end
   end
 
@@ -135,13 +136,19 @@ class ActivityTrackingService
   #
   def send_activity_email(recipient, question_events, question_tags, events)
     puts "Send mail to:" + recipient.email
-    mail = ActivityTrackingMailer.create_activity_tracking_email(recipient,
-                                                                 question_events,
-                                                                 question_tags,
-                                                                 events)
+    mail = ActivityTrackingMailer.create_activity_tracking_email(recipient,question_events,question_tags,events)
     ActivityTrackingMailer.deliver(mail)
   end
 
   #handle_asynchronously :send_activity_email
 
+
+
+  def set_titles_hash(documents)
+    hash = Hash.new
+    documents.each do |document|
+      hash[document.language_id] = document.title
+    end
+    hash
+  end
 end
