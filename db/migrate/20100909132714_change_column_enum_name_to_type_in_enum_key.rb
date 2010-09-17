c3c8a3807d3103eae5353aabc2780fa383515546
unknown <Tiago@cardoso@echologic.org>
2010-09-17 12:32:08 +0200

    more changes to migrations

--- db/migrate/20100909132714_change_column_enum_name_to_type_in_enum_key.rb
+++ db/migrate/20100909132714_change_column_enum_name_to_type_in_enum_key.rb
@@ -1,9 +1,18 @@
 class ChangeColumnEnumNameToTypeInEnumKey < ActiveRecord::Migration
   def self.up
     rename_column :enum_keys, :enum_name, :type
+    
+    EnumKey.all.each do |enum_key|
+      enum_key.type = enum_key.type.singularize.classify 
+      enum_key.save
+    end
   end
 
   def self.down
     rename_column :enum_keys, :type, :enum_name
+    EnumKey.all.each do |enum_key|
+      enum_key.enum_name = enum_key.enum_name.underscore.pluralize
+      enum_key.save
+    end
   end
 end
--- db/migrate/20100910131733_change_column_language_id_to_code_from_enum_values.rb
+++ db/migrate/20100910131733_change_column_language_id_to_code_from_enum_values.rb
@@ -1,9 +1,12 @@
 class ChangeColumnLanguageIdToCodeFromEnumValues < ActiveRecord::Migration
   def self.up
+    enum_values = EnumValue.all
     rename_column :enum_values, :language_id, :code
     change_column :enum_values, :code, :string
-    
-    Rake::Task['db:seed'].invoke
+    EnumValue.all.each_with_index do |enum_value, i|
+      enum_value.code = EnumKey.find_by_type_and_key("Language",enum_values[0].language_id).code
+      enum_value.save
+    end
   end
 
   def self.down
