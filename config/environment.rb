77a38b272dfe75a5bd33a07de14fa00531dfa77b
Laszlo Papp <laszlo.papp@echologic.org>
2010-09-21 18:50:29 +0200

    Dependency: will_paginate (without mislav-)

--- config/environment.rb
+++ config/environment.rb
@@ -48,10 +48,10 @@ Rails::Initializer.run do |config|
 
   # gem for running the background jobs in production
   config.gem 'daemons'
-  
+
   # gems for endless pagination
-  config.gem 'mislav-will_paginate', :lib => 'will_paginate', :source => 'http://gems.github.com'
-  
+  config.gem 'will_paginate', :lib => 'will_paginate', :source => 'http://gems.github.com'
+
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
