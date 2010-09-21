5fd54cdce92676c93ea66a395d9467e3aeb1dc84
Laszlo Papp <laszlo.papp@echologic.org>
2010-09-21 18:49:53 +0200

    Fixed migration (uses direct SQL).

--- db/migrate/20100910131733_change_column_language_id_to_code_from_enum_values.rb
+++ db/migrate/20100910131733_change_column_language_id_to_code_from_enum_values.rb
@@ -1,17 +1,18 @@
 class ChangeColumnLanguageIdToCodeFromEnumValues < ActiveRecord::Migration
   def self.up
-    change_column :enum_values, :language_id, :string
-    rename_column :enum_values, :language_id, :code
-    
-    Rake::Task['db:seed'].invoke
-#    EnumValue.find(:all).each_with_index do |enum_value, i|
-#      enum_value.code = Language.find_by_key(enum_value.code.to_i).code
-#      enum_value.save(false)
-#    end
+    #add_column :enum_values, :code, :string
+    Language.all.each do |language|
+      execute "UPDATE enum_values SET code = '#{language.code}' WHERE language_id = #{language.id}"
+    end
+    remove_column :enum_values, :language_id
+    add_index :enum_values, [:enum_key_id, :code, :id], :name => "idx_enum_values_enum_key_code_pk"
   end
 
   def self.down
-    change_column :enum_values, :code, :integer
-    rename_column :enum_values, :code, :language_id
+    add_column :enum_values, :language_id, :integer
+    Language.all.each do |language|
+      execute "UPDATE enum_values SET language_id = #{language.id} WHERE code = '#{language.code}'"
+    end
+    remove_column :enum_values, :code
   end
 end
