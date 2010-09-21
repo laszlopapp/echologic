17a333b07010bc85f937e646700598e9d63a7119
Laszlo Papp <laszlo.papp@echologic.org>
2010-09-21 20:40:09 +0200

    Added GitHub URL.

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,8 @@
 RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 MAX_SESSION_PERIOD = 3600
 
+GITHUB_URL = 'http://github.com/echo-on-waves/echologic'
+
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
