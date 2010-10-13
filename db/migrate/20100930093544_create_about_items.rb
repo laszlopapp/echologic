aaf23298c06618c053717ba0d6ecf2fa186f2b69
unknown <Tiago@cardoso@echologic.org>
2010-10-13 16:38:54 +0200

    added migration for the collaboration team seeds

--- db/migrate/20100930093544_create_about_items.rb
+++ db/migrate/20100930093544_create_about_items.rb
@@ -17,10 +17,47 @@ class CreateAboutItems < ActiveRecord::Migration
       t.string :locale
       t.text :description
     end
+    
+    #add new seeds
+    CollaborationTeam.enumeration_model_updates_permitted = true
+    CollaborationTeam.purge_enumerations_cache
+    %w(core_team supporters alumni technology_partners financial_partners).each_with_index do |code, index|
+      CollaborationTeam.create!(:code => code, :key => index+1, :description => "collaboration_team")
+    end
+    CollaborationTeam.enumeration_model_updates_permitted = false
+    
+    EnumValue.enumeration_model_updates_permitted = true
+    ["Core Team","Ständiges Team","Coeur équipe","Equipa-Núcleo","Equipo Permanente"].each_with_index do |value,index|
+      EnumValue.create!(:enum_key => EnumKey.find_by_code_and_type('core_team','CollaborationTeam'), :code => EnumKey.find_by_type_and_key('Language',index+1).code, :value => value, :context=> "")
+    end
+    ["Supporters","Unterstützer","Collaborateurs","Colaboradores","Colaboradores"].each_with_index do |value,index|
+      EnumValue.create!(:enum_key => EnumKey.find_by_code_and_type('supporters','CollaborationTeam'), :code => EnumKey.find_by_type_and_key('Language',index+1).code, :value => value, :context=> "")
+    end
+    ["Alumni","Absolventen","Diplômées","Graduados","Graduados"].each_with_index do |value,index|
+      EnumValue.create!(:enum_key => EnumKey.find_by_code_and_type('alumni','CollaborationTeam'), :code => EnumKey.find_by_type_and_key('Language',index+1).code, :value => value, :context=> "")
+    end
+    ["Technology Partners","Technologiepartner","Partenaires Tecnologiques","Parceiros Tecnológicos","Colaboradores Tecnológicos"].each_with_index do |value,index|
+      EnumValue.create!(:enum_key => EnumKey.find_by_code_and_type('technology_partners','CollaborationTeam'), :code => EnumKey.find_by_type_and_key('Language',index+1).code, :value => value, :context=> "")
+    end
+    ["Financial Partners","Finanzpartner","Partenaires Financières","Parceiros Financeiros","Colaboradores Financieros"].each_with_index do |value,index|
+      EnumValue.create!(:enum_key => EnumKey.find_by_code_and_type('financial_partners','CollaborationTeam'), :code => EnumKey.find_by_type_and_key('Language',index+1).code, :value => value, :context=> "")
+    end
+    EnumValue.purge_enumerations_cache
+    EnumValue.enumeration_model_updates_permitted = false
   end
 
   def self.down
     drop_table :about_items
     drop_table :about_item_translations
+    EnumValue.enumeration_model_updates_permitted = true
+    CollaborationTeam.all.each do |c|
+      EnumValue.destroy_all({:enum_key_id => c.id})
+    end
+    EnumValue.purge_enumerations_cache
+    EnumValue.enumeration_model_updates_permitted = false
+    CollaborationTeam.enumeration_model_updates_permitted = true
+    CollaborationTeam.destroy_all
+    CollaborationTeam.purge_enumerations_cache
+    CollaborationTeam.enumeration_model_updates_permitted = false
   end
 end
