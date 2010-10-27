4a42dd384816bc1d2b452b5770b66963d4470fb1
Laszlo Papp <laszlo.papp@echologic.org>
2010-10-27 20:09:50 +0200

    Renamed to AboutCategory + new enum creation moved back to seeds.

--- db/migrate/20100930093544_create_about_items.rb
+++ db/migrate/20100930093544_create_about_items.rb
@@ -6,58 +6,35 @@ class CreateAboutItems < ActiveRecord::Migration
       t.integer :photo_file_size
       t.string :name
       t.text :description
-      t.integer :collaboration_team_id
+      t.integer :about_category_id
       t.integer :index
       t.timestamps
     end
-    
-    # Descriptions' translations table
+
+    # Translation table for descriptions
     create_table :about_item_translations do |t|
       t.integer :about_item_id
       t.string :locale
       t.text :description
     end
-    
-    #add new seeds
-    CollaborationTeam.enumeration_model_updates_permitted = true
-    CollaborationTeam.purge_enumerations_cache
-    %w(core_team supporters alumni technology_partners financial_partners).each_with_index do |code, index|
-      CollaborationTeam.create!(:code => code, :key => index+1, :description => "collaboration_team")
-    end
-    CollaborationTeam.enumeration_model_updates_permitted = false
-    
-    EnumValue.enumeration_model_updates_permitted = true
-    ["Core Team","Ständiges Team","Coeur équipe","Equipa-Núcleo","Equipo Permanente"].each_with_index do |value,index|
-      EnumValue.create!(:enum_key => EnumKey.find_by_code_and_type('core_team','CollaborationTeam'), :code => EnumKey.find_by_type_and_key('Language',index+1).code, :value => value, :context=> "")
-    end
-    ["Supporters","Unterstützer","Collaborateurs","Colaboradores","Colaboradores"].each_with_index do |value,index|
-      EnumValue.create!(:enum_key => EnumKey.find_by_code_and_type('supporters','CollaborationTeam'), :code => EnumKey.find_by_type_and_key('Language',index+1).code, :value => value, :context=> "")
-    end
-    ["Alumni","Absolventen","Diplômées","Graduados","Graduados"].each_with_index do |value,index|
-      EnumValue.create!(:enum_key => EnumKey.find_by_code_and_type('alumni','CollaborationTeam'), :code => EnumKey.find_by_type_and_key('Language',index+1).code, :value => value, :context=> "")
-    end
-    ["Technology Partners","Technologiepartner","Partenaires Tecnologiques","Parceiros Tecnológicos","Colaboradores Tecnológicos"].each_with_index do |value,index|
-      EnumValue.create!(:enum_key => EnumKey.find_by_code_and_type('technology_partners','CollaborationTeam'), :code => EnumKey.find_by_type_and_key('Language',index+1).code, :value => value, :context=> "")
-    end
-    ["Financial Partners","Finanzpartner","Partenaires Financières","Parceiros Financeiros","Colaboradores Financieros"].each_with_index do |value,index|
-      EnumValue.create!(:enum_key => EnumKey.find_by_code_and_type('financial_partners','CollaborationTeam'), :code => EnumKey.find_by_type_and_key('Language',index+1).code, :value => value, :context=> "")
-    end
-    EnumValue.purge_enumerations_cache
-    EnumValue.enumeration_model_updates_permitted = false
+
+    # Add new enums for AboutCategory
+    Rake::Task['db:seed'].invoke
   end
+
 
   def self.down
     drop_table :about_items
     drop_table :about_item_translations
     EnumValue.enumeration_model_updates_permitted = true
-    CollaborationTeam.all.each do |c|
+    AboutCategory.all.each do |c|
       EnumValue.destroy_all({:enum_key_id => c.id})
     end
     EnumValue.purge_enumerations_cache
     EnumValue.enumeration_model_updates_permitted = false
-    CollaborationTeam.enumeration_model_updates_permitted = true
-    CollaborationTeam.destroy_all
-    CollaborationTeam.purge_enumerations_cache
-    CollaborationTeam.enumeration_model_updates_permitted = false
+    AboutCategory.enumeration_model_updates_permitted = true
+    AboutCategory.destroy_all
+    AboutCategory.purge_enumerations_cache
+    AboutCategory.enumeration_model_updates_permitted = false
   end
 end
