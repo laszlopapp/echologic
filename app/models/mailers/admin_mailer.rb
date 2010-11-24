b83d9ef6b89e27ed6c2730282475a70bd509f32b
Laszlo Papp <laszlo.papp@echologic.org>
2010-11-24 16:49:16 +0100

    Renamed variable (language).

--- app/models/mailers/admin_mailer.rb
+++ app/models/mailers/admin_mailer.rb
@@ -1,14 +1,13 @@
 class AdminMailer < ActionMailer::Base
 
-  # Send a feedback object as email to the FEEDBACK_RECIPIENT specified
-  # in the environment.
+  # Send a newletter to the given user.
   def newsletter(user, subject, text)
-    default = user.default_language
+    language = user.default_language
     subject       subject
     recipients    user.email
     from          "newsletter@echologic.org"
     sent_on       Time.now
     content_type  "text/html"
-    body          :name => user.full_name, :text => text, :language => default
+    body          :name => user.full_name, :text => text, :language => language
   end
 end
