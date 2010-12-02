3bb9c9dd7942a138d059b874d6b55ce099de547a
Laszlo Papp <laszlo.papp@echologic.org>
2010-12-02 20:23:50 +0100

    Use the general HTML mail layout and helper.

--- app/models/mailers/activity_tracking_mailer.rb
+++ app/models/mailers/activity_tracking_mailer.rb
@@ -1,10 +1,12 @@
 class ActivityTrackingMailer < ActionMailer::Base
   include StatementsHelper
+  layout 'mail'
+  helper :mail
 
   # Send the activities on the subscribed objects to the subscribable
-  def activity_tracking_email(recipient,question_events, question_tags, events)
-    default = recipient.default_language
-    subject       I18n.t('mailers.activity_tracking.subject', :locale => default.code)
+  def activity_tracking_mail(recipient,question_events, question_tags, events)
+    language = recipient.default_language
+    subject       I18n.t('mailers.activity_tracking.subject', :locale => language.code)
     from          "noreply@echologic.org"
     recipients    recipient.email
     sent_on       Time.now
@@ -12,7 +14,7 @@ class ActivityTrackingMailer < ActionMailer::Base
     body          :question_events => question_events,
                   :question_tags => question_tags,
                   :events => events,
-                  :language => default,
+                  :language => language,
                   :preferred_language_ids => recipient.sorted_spoken_language_ids
   end
 
--- app/models/mailers/newsletter_mailer.rb
+++ app/models/mailers/newsletter_mailer.rb
@@ -1,11 +1,13 @@
 class NewsletterMailer < ActionMailer::Base
+  layout 'mail'
+  helper :mail
 
   # Send a newletter to the given user.
-  def newsletter(recipient, subject, text)
+  def newsletter_mail(recipient, subject, text)
     language = recipient.default_language
     subject       subject
     recipients    recipient.email
-    from          "newsletter@echologic.org"
+    from          "noreply@echologic.org"
     sent_on       Time.now
     content_type  "text/html"
     body          :name => recipient.full_name,
