aec7bf95457e13b7d0b23ac6be7c309eaf29e6c1
Laszlo Papp <laszlo.papp@echologic.org>
2010-12-04 00:08:48 +0100

    Newsletter is on by default.

--- db/migrate/20101203223755_change_newsletter_notification_default.rb
+++ db/migrate/20101203223755_change_newsletter_notification_default.rb
@@ -1,7 +1,9 @@
 class ChangeNewsletterNotificationDefault < ActiveRecord::Migration
   def self.up
+    change_column_default :users, :newsletter_notification, 1
   end
 
   def self.down
+    change_column_default :users, :newsletter_notification, 0
   end
 end
