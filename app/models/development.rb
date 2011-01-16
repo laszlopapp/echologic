a29b81dae510e3ab7f329266151a525c847ec15c
Laszlo Papp <laszlo.papp@echologic.org>
2011-01-17 00:21:14 +0100

    Increased readyness.

--- app/models/development.rb
+++ app/models/development.rb
@@ -1,8 +1,8 @@
 class Development
 
   # Roadmap progress constants
-  DISCUSS_PROGRESS = 25
-  CONNECT_PROGRESS = 5
+  DISCUSS_PROGRESS = 35
+  CONNECT_PROGRESS = 10
   ACT_PROGRESS = 0
   MY_ECHO_PROGRESS = 10
 
