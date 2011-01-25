b55396c524a58a004fd025d4ce94e7010d286152
unknown <Tiago@cardoso@echologic.org>
2011-01-25 12:56:24 +0100

    corrected the name of a migration

--- db/migrate/20101203223759_add_question_id_to_statement_node.rb
+++ db/migrate/20101203223759_add_question_id_to_statement_node.rb
@@ -1,4 +1,4 @@
-class AddDiscussionIdToStatementNode < ActiveRecord::Migration
+class AddQuestionIdToStatementNode < ActiveRecord::Migration
   def self.up
     add_column :statement_nodes, :question_id, :integer
   end
