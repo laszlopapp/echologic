d298ec810b9e3a9222045dd0fcfcacca6b866867
Laszlo Papp <laszlo.papp@echologic.org>
2011-01-28 23:10:46 +0100

    Cleaned up.

--- public/javascripts/discuss/breadcrumbs.js
+++ public/javascripts/discuss/breadcrumbs.js
@@ -2,19 +2,27 @@
 
   $.fn.breadcrumbs = function(currentSettings){
 
-    $.fn.breadcrumb.defaults = {
+    $.fn.breadcrumbs.defaults = {
+      'container_animation_params' : {
+        'width' : 'toggle',
+        'opacity': 'toggle'
+      },
+      'breadcrumb_animation_params' : {
+        'width' : 'toggle',
+        'opacity': 'toggle'
+      },
       'animation_speed': 500
     };
 
 		// Merging settings with defaults
-    var settings = $.extend({}, $.fn.breadcrumb.defaults, currentSettings);
+    var settings = $.extend({}, $.fn.breadcrumbs.defaults, currentSettings);
 
     // Creating and binding the breadcrumb API
     var api = this.data('breadcrumbApi');
     if (api) {
       api.reinitialise(settings);
     } else {
-    api = new Breadcrumb();
+    api = new Breadcrumbs(this);
       this.data('breadcrumbApi', api);
     }
     return this;
@@ -24,7 +32,7 @@
     /* The breadcrumbs handler */
     /***************************/
 
-    function Breadcrumb(breadcrumbs) {
+    function Breadcrumbs(breadcrumbs) {
       initialise();
 
 			function initialise(){
@@ -37,23 +45,21 @@
 
 				var width = updateContainerWidth();
 				jsp.reinitialise();
-        jsp.scrollByX(width);
+        jsp.scrollToX(width);
 
 				var elements = jsp.getContentPane().find('.elements');
-				if (elements.children().length == 0) {breadcrumbs.fadeOut(settings['animation_speed']);}
+				if (elements.children().length == 0) {
+          toggleContainer();
+        }
 	    }
-
-		  function initBreadcrumb(breadcrumb) {
-				if (!breadcrumb.hasClass('.search_link')) {
-					initBreadcrumbLink(breadcrumb);
-				}
-			}
 
 
       /*
        * Initializes the links in the different sort of breadcrumbs.
        */
-			function initBreadcrumbLink(breadcrumb) {
+			function initBreadcrumb(breadcrumb) {
+
+        if (breadcrumb.hasClass('.search_link')) {return;}
 
         // Loads ids of the statements appearing in the stack
 		    var path_id = breadcrumb.attr('id');
@@ -95,7 +101,6 @@
 		    });
 		  }
 
-
 			function updateContainerWidth() {
 				var jsp = breadcrumbs.data('jsp');
         var elements = jsp.getContentPane().find(".elements");
@@ -109,6 +114,26 @@
 				return width;
 			}
 
+
+      /*
+       * Shows / hides the breadcrumbs container with all its breadcrumbs
+       * (actually, there are no breadcrumbs when it gets hidden).
+       */
+      function toggleContainer() {
+        breadcrumbs.animate(settings['container_animation_speed'],
+                            settings['animation_speed']);
+      }
+
+
+      /*
+       * Shows / hides the given breadcrumb.
+       */
+      function toggleBreadcrumb(breadcrumb) {
+        breadcrumb.animate(settings['breadcrumb_animation_speed'],
+                           settings['animation_speed']);
+      }
+
+
 			// Public API
       $.extend(this,
       {
@@ -117,23 +142,28 @@
           initialise();
         },
 
-				addBreadcrumbs : function(attrs_array) {
+				addBreadcrumbs : function(breadcrumbsData) {
 
 					var jsp = breadcrumbs.data('jsp');
           var elements = jsp.getContentPane().find(".elements");
 
-					if (attrs_array) {
-						if(breadcrumbs.is(":hidden")) {breadcrumbs.fadeIn(settings['animation_speed']);}
-			  	  // load new breadcrumb entries
-						$.each(attrs_array, function(index, attrs){
+					if (breadcrumbsData) {
+						if(breadcrumbs.is(":hidden")) {
+              toggleContainer();
+            }
+			  	  // Assemble new breadcrumb entries
+						$.each(breadcrumbsData, function(index, breadcrumbData) {
 							var breadcrumb = $('<div/>').addClass('breadcrumb');
-							breadcrumb.append($('<span/>').addClass('label').text(attrs[4]));
-							breadcrumb.append($('<span/>').addClass('over').text(attrs[5]).hide());
-							if (!index == 0 || elements.find(".breadcrumb").length != 0) {
-								var del = $("<span/>").addClass('delimiter');
-								breadcrumb.append(del);
+							breadcrumb.append($('<span/>').addClass('label').text(breadcrumbData[4]));
+							breadcrumb.append($('<span/>').addClass('over').text(breadcrumbData[5]));
+							if (index != 0 || elements.find(".breadcrumb").length != 0) {
+								breadcrumb.append($("<span/>").addClass('delimiter'));
 							}
-							breadcrumb.append($('<a/>').attr('id', attrs[0]).addClass(attrs[1]).attr('href', attrs[2]).text(attrs[3]));
+							breadcrumb.append($('<a/>').
+                         attr('id', breadcrumbData[0]).
+                         addClass(breadcrumbData[1]).
+                         attr('href', breadcrumbData[2]).
+                         text(breadcrumbData[3]));
 							breadcrumb.hide();
 							initBreadcrumb(breadcrumb.find('a'));
 							elements.append(breadcrumb);
@@ -142,19 +172,20 @@
 					var width = updateContainerWidth();
 
 					jsp.reinitialise();
-					jsp.scrollByX(width);
-
-					elements.find('.breadcrumb:hidden').fadeIn(settings['animation_speed']);
+					jsp.scrollToX(width);
+				  elements.find('.breadcrumb:hidden').each(function() {
+            toggleBreadcrumb($(this));
+          });
 				},
 
 				deleteAfter : function (originId) {
 					var jsp = breadcrumbs.data('jsp');
 					var elements = jsp.getContentPane().find('.elements');
 					if (originId.length > 0) {
-            // there is an origin, so delete breadcrumbs to the right
+            // There is an origin, so delete breadcrumbs to the right
 				  	elements.find('a#' + originId).parent().nextAll().remove();
 				  } else {
-            // no origin, that means first breadcrumb pressed, no predecessor, so delete everything
+            // No origin, that means first breadcrumb pressed, no predecessor, so delete everything
 						elements.find('a').each(function() {
 						  $(this).parent().remove();
 						});
@@ -163,33 +194,37 @@
 					updateContainerWidth();
 					jsp.scrollToX(0);
 					jsp.reinitialise();
-					if (jsp.getContentPane().find('a').length == 0) {breadcrumbs.fadeOut(settings['animation_speed']);}
+					if (jsp.getContentPane().find('a').length == 0) {
+            toggleContainer();
+          }
 					return this;
 				},
 
 				breadcrumbsToLoad : function (bids) {
 		      if (bids == null) { return []; }
-		      // Current bids in stack
-		      var bids_stack = bids.split(",");
+		      // Current bids in the list
+		      var bid_list = bids.split(",");
 
 		      // Current breadcrumb entries
-		      var visible_bids = breadcrumbs.find("a").map(function(){
+		      var visible_bids = breadcrumbs.find("a").map(function() {
 						return this.id;
 		      }).get();
 
-		      // Get bids that are not visible (don't repeat yourself)
-          return $.grep(bids_stack, function(a) {
+		      // Get bids that are not visible (DRY)
+          return $.grep(bid_list, function(a) {
 		        return $.inArray(a, visible_bids) == -1;
           });
 		    },
 
-				getBreadcrumbStack : function (new_breadcrumb){
-		      var current_breadcrumbs = breadcrumbs.find(".breadcrumb a").map(function(){
+				getBreadcrumbStack : function (newBreadcrumb = null) {
+		      var currentBreadcrumbs = breadcrumbs.find(".breadcrumb a").map(function() {
 						return this.id;
 					}).get();
-					if (new_breadcrumb) {current_breadcrumbs.push(new_breadcrumb);}
+					if (newBreadcrumb) {
+            currentBreadcrumbs.push(newBreadcrumb);
+          }
 
-		      return current_breadcrumbs;
+		      return currentBreadcrumbs;
 		    }
 			});
     }
--- public/javascripts/discuss/statement_form.js
+++ public/javascripts/discuss/statement_form.js
@@ -1,44 +1,63 @@
-(function($, window, undefined){
+(function($) {
 
-  $.fn.statement_form = function(settings) {
+  $.fn.statementForm = function(currentSettings) {
 
-    function StatementForm(stForm){
-	    var statementForm = stForm; 
-			
+    $.fn.statementForm.defaults = {
+      'animation_speed': 500,
+      'taggableClass' : 'taggable'
+    };
+
+    // Merging settings with defaults
+    var settings = $.extend({}, $.fn.statementForm.defaults, currentSettings);
+
+    // Creating and binding the statement form API
+    var api = this.data('statementFormApi');
+    if (api) {
+      api.reinitialise(settings);
+    } else {
+      api = new StatementForm();
+      this.data('statementFormApi', api);
+    }
+    return this;
+
+
+    /******************************/
+    /* The statement form handler */
+    /******************************/
+    function StatementForm() {
+
 			initialise();
-			
+
 			function initialise(){
 			  loadRTEEditor();
 
-        /*New Statement Form Helpers */
-        if (elem.hasClass('new')) {
+        // New Statement Form Helpers
+        if (this.hasClass('new')) {
           hideNewStatementType();
           loadDefaultText();
           handleStatementFormsSubmit();
           initFormCancelButton();
-          
+
         }
 
-        /* Taggable Form Helpers */
-        if (elem.hasClass(settings['taggableClass'])) {
-          elem.taggable();
+        // Taggable Form Helpers
+        if (this.hasClass(settings['taggableClass'])) {
+          this.taggable();
         }
-        
-        if (elem.hasClass('follow_up_question')) {
-					initFollowUpQuestionFormEvents();
+
+        if (this.hasClass('follow_up_question')) {
+					initCancelFUQLink();
         }
 			}
-			
-			//Auxiliary Functions
-			
+
 			/*
-       * loads the statement text RTE editor
+       * Loads the Rich Text Editor for the statement text.
        */
       function loadRTEEditor() {
-        var textArea = statementForm.find('textarea.rte_doc, textarea.rte_tr_doc');
-        defaultText = textArea.attr('data-default');
-    
-        url = 'http://' + window.location.host + '/stylesheets/';
+        var textArea = this.find('textarea.rte_doc, textarea.rte_tr_doc');
+        var defaultText = textArea.attr('data-default');
+        var url = 'http://' + window.location.host + '/stylesheets/';
+
         textArea.rte({
           css: ['jquery.rte.css'],
           base_url: url,
@@ -46,31 +65,40 @@
           controls_rte: rte_toolbar,
           controls_html: html_toolbar
         });
-        statementForm.find('.focus').focus();
-    
-        /* for default text */
-        statementForm.find('iframe').attr('data-default', defaultText);
+        this.find('.focus').focus();
+
+        // The default placeholder text
+        this.find('iframe').attr('data-default', defaultText);
       }
-			
+
+
+      /*
+       * Shows the statement type on new statement forms
+       */
+      function showNewStatementType() {
+        var input_type = this.find('input#type');
+        input_type.attr('value', input_type.data('value'));
+      }
+
+
 			/*
-       * Hides the statement type on new statement forms
+       * Hides the statement type on new statement forms.
        */
       function hideNewStatementType() {
-        input_type = statementForm.find('input#type');
-        input_type.data('value',input_type.attr('value'));
+        var input_type = this.find('input#type');
+        input_type.data('value', input_type.attr('value'));
         input_type.removeAttr('value');
       }
-			
-			
+
+
 			/*
-       * Loads Form's Default Text to title, text and tags
+       * Loads the form's default texts for title, text and tags.
        */
 			function loadDefaultText() {
-        if (!statementForm.hasClass('new')) {return;}
-    
-    
-        /* Text Inputs */
-        var inputText = statementForm.find("input[type='text']");
+        if (!this.hasClass('new')) {return;}
+
+        // Text Inputs
+        var inputText = this.find("input[type='text']");
         var value = inputText.attr('data-default');
         if (inputText.val().length == 0) {
           inputText.toggleVal({
@@ -80,32 +108,31 @@
         }
         inputText.removeAttr('data-default');
         inputText.blur();
-    
-    
-        /* Text Area (RTE Editor) */
-        var editor = statementForm.find("iframe.rte_doc");
-          var value = editor.attr('data-default');
+
+
+        // Text Area (RTE Editor)
+        var editor = this.find("iframe.rte_doc");
+        value = editor.attr('data-default');
         var doc = editor.contents().get(0);
-        text = $(doc).find('body');
-        if(text.html().length == 0 || html.val() == '</br>') {
-          label = $("<span class='defaultText'></span>").html(value);
+        var text = $(doc).find('body');
+        if(text.html().length == 0 || text.html().val() == '</br>') {
+          var label = $("<span class='defaultText'></span>").html(value);
           label.insertAfter(editor);
-    
+
           $(doc).bind('click', function(){
             label.hide();
           });
           $(doc).bind('blur', function(){
-            new_text = $(editor.contents().get(0)).find('body');
-            if (new_text.html().length == 0 || new_text.html() == '</br>') {
+            var new_text = $(editor.contents().get(0)).find('body');
+            if (new_text.html().length == 0 || new_text.html().val() == '</br>') {
               label.show();
             }
           });
         }
         editor.removeAttr('data-default');
-    
-    
-        /* Clean text inputs on submit */
-        statementForm.bind('submit', (function() {
+
+        // Clean text inputs on submit
+        this.bind('submit', (function() {
           $(this).find(".toggleval").each(function() {
             if($(this).val() == $(this).data("defText")) {
               $(this).val("");
@@ -113,117 +140,93 @@
           });
         }));
       }
-			
-			
+
+      /*
+       * Submits the form.
+       */
 			function handleStatementFormsSubmit() {
-        statementForm.bind('submit', (function(){
+        this.bind('submit', (function() {
           showNewStatementType();
           $.ajax({
             url: this.action,
             type: "POST",
             data: $(this).serialize(),
             dataType: 'script',
-            success: function(data, status){
+            success: function() {
               hideNewStatementType();
             }
           });
           return false;
         }));
       }
-			
+
 			/*
-       * Handles Cancel Button click on new statement forms
+       * Handles Cancel Button click on new statement forms.
        */
       function initFormCancelButton() {
-        var cancelButton = statementForm.find('.buttons a.cancel');
+        var cancelButton = this.find('.buttons a.cancel');
         if ($.fragment().sids) {
           var sids = $.fragment().sids;
           var new_sids = sids.split(",");
           var path = "/" + new_sids[new_sids.length-1];
-    
           new_sids.pop();
-    
+
           cancelButton.addClass("ajax");
           cancelButton.attr('href', $.queryString(cancelButton.attr('href').replace(/\/\d+/, path), {
-            "sids": new_sids.join(","), 
+            "sids": new_sids.join(","),
 						"origin": $.fragment().origin
           }));
         }
       }
-			
-			
-        
-      function initFollowUpQuestionFormEvents() {
-        statementForm.find("a.cancel_text_button").bind("click", function(){
+
+      function initCancelFUQLink() {
+        this.find("a.cancel_text_button").bind("click", function(){
           var bids = $('#breadcrumbs').data('breadcrumbApi').getBreadcrumbStack(null);
-					
-          /* get last breadcrumb id */
+
+          // Get last breadcrumb id
 					var last_bid = bids[bids.length-1];
-					
-          /* get last statement view id (if teaser, parent id + '/' */
+
+          // Get last statement view id (if teaser, parent id + '/'
           var last_sid = $.fragment().sids;
           if (last_sid) {
             last_sid = $.fragment().sids.split(',').pop().match(/\d+\/?/).shift();
           } else {
             last_sid = '';
           }
-					
-					
-					
-					if (getStatementId(last_bid).match(last_sid)) { /* create follow up question button in children had been pressed */
+
+					if (getStatementId(last_bid).match(last_sid)) {
+            // Create follow up question button in children had been pressed
 					  var origin_bid = $('#breadcrumbs a.statement:last').parent().prev().find('a').attr('id');
             bids.pop();
-            $.setFragment({ "bids": bids.join(','), "new_level": true, "origin": origin_bid });
-            return false;
-          } else { /* create follow up question button in siblings had been pressed */
-					  $.setFragment({ "bids": '', "new_level": true, "origin": last_bid });
-            return false;
+            $.setFragment({
+              "bids": bids.join(','),
+              "new_level": true,
+              "origin": origin_bid
+            });
+          } else {
+            // Create follow up question button in siblings had been pressed
+					  $.setFragment({
+              "bids": '',
+              "new_level": true,
+              "origin": last_bid
+            });
+
 					}
+          return false;
         });
       }
-			
-			/*
-       * Shows the statement type on new statement forms
-       */
-      function showNewStatementType() {
-        input_type = statementForm.find('input#type');
-        input_type.attr('value', input_type.data('value'));
-      }
-			
-			// API Functions
-			
-			$.extend(this, 
+
+
+			// Public API functions
+			$.extend(this,
       {
-        reinitialise: function(s)
+        reinitialise: function()
         {
-          s = $.extend({}, s, settings);
-          initialise(s);
+          initialise();
         }
 			});
+
 		}
 
-
-    $.fn.statement_form.defaults = {
-      'animation_speed': 500,
-			'taggableClass' : 'taggable'
-    };
-
-    // Pluginifying code...
-    settings = $.extend({}, $.fn.statement_form.defaults, settings);
-
-    var ret;
-    
-    var elem = $(this), api = elem.data('statementFormApi');
-    if (api) {
-      api.reinitialise(settings);
-    } else {
-    api = new StatementForm(elem);
-      elem.data('statementFormApi', api);
-    }
-    ret = ret ? ret.add(elem) : elem;
-    
-    return ret;
-    
-    
   };
-})(jQuery,this);
+})(jQuery);
