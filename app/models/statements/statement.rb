d26fddc47a30b82d2c3d125a321b1dc8b9bec930
Laszlo Papp <laszlo.papp@echologic.org>
2011-02-01 18:00:41 +0100

    Formating.

--- app/models/statements/statement.rb
+++ app/models/statements/statement.rb
@@ -4,15 +4,15 @@ class Statement < ActiveRecord::Base
   has_many :statement_documents, :dependent => :destroy
   belongs_to :statement_image
   delegate :image, :image=, :to => :statement_image
-  
+
   has_enumerated :editorial_state, :class_name => 'StatementState'
-  
+
   validates_presence_of :editorial_state_id
   validates_numericality_of :editorial_state_id
   validates_associated :statement_documents
   validates_associated :statement_image
 
-  
+
 
   has_many :statement_histories, :source => :statement_histories
 
@@ -31,21 +31,23 @@ class Statement < ActiveRecord::Base
   has_enumerated :original_language, :class_name => 'Language'
 
   named_scope :find_by_title, lambda {|value|
-            { :include => :statement_documents, :conditions => ['statement_documents.title LIKE ? and statement_documents.current = 1', "%#{value}%"] } }
+            { :include => :statement_documents,
+              :conditions => ['statement_documents.title LIKE ? and statement_documents.current = 1', "%#{value}%"] } }
 
 
   #
   # Returns the current statement document in the given language.
   #
   def document_in_language(language)
-    self.statement_documents.find(:first, :conditions => ["language_id = ? and current = 1", language.id])
+    self.statement_documents.find(:first,
+                                  :conditions => ["language_id = ? and current = 1", language.id])
   end
-  
-  
+
+
   ###################
   # PUBLISH ACTIONS #
   ###################
-  
+
   # static for now
   def published?
     self.editorial_state == StatementState["published"]
@@ -55,8 +57,8 @@ class Statement < ActiveRecord::Base
   def publish
     self.editorial_state = StatementState["published"]
   end
-  
+
   def filtered_topic_tags
-    self.topic_tags.select{|tag|tag.index('*') != 0}
+    self.topic_tags.select{|tag| !tag.starts_with?('*')}
   end
 end
