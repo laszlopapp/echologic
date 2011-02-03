a399b8645f5a39ba645d8a028498cfc7ae51d94a
Laszlo Papp <laszlo.papp@echologic.org>
2011-02-03 20:10:26 +0100

    Fixed default placeholder text initialization for tags.

--- public/javascripts/discuss/statement_form.js
+++ public/javascripts/discuss/statement_form.js
@@ -95,17 +95,16 @@
         if (!form.hasClass('new')) {return;}
 
         // Text Inputs
-        var inputText = form.find("input[type='text']");
-        var value = inputText.attr('data-default');
-        if (inputText.val().length == 0) {
-          inputText.toggleVal({
-            populateFrom: 'custom',
-            text: value
-          });
-        }
-        inputText.removeAttr('data-default');
-        inputText.blur();
-
+        form.find("input[type='text']").each(function() {
+          var inputText = $(this);
+          var value = inputText.attr('data-default');
+          if (inputText.val().length == 0) {
+            inputText.toggleVal({
+              populateFrom: 'custom',
+              text: value
+            });
+          }
+        }).removeAttr('data-default').blur();
 
         // Text Area (RTE Editor)
         var editor = form.find("iframe.rte_doc");
