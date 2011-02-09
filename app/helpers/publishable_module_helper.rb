b289b72fca90330a84e4290f295dc37de8c469c3
unknown <Tiago@cardoso@echologic.org>
2011-02-09 18:33:00 +0100

    recorrected the corrections

--- app/helpers/publishable_module_helper.rb
+++ app/helpers/publishable_module_helper.rb
@@ -38,13 +38,13 @@ module PublishableModuleHelper
   #
   # question link for the discuss search results
   #
-  def link_to_question(title, question,image,long_title,search_terms=nil)
+  def link_to_question(title, question,long_title,search_terms=nil)
     search_terms = search_terms.nil? ? '' : search_terms.gsub(/,/,'\\;')
     origin = search_terms.blank? ? "ds" : "sr#{search_terms}"
     link_to statement_node_url(question, :origin => origin, :bids => origin),
                :title => "#{h(title) if long_title}",
                :class => "avatar_holder#{' ttLink no_border' if long_title }" do
-      image_tag image.url(:small)
+      image_tag question.image.url(:small)
     end
   end
 
