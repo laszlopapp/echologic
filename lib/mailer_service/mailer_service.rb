8842c046bac9836ba2a3da0bb57c59dc404bae11
Laszlo Papp <laszlo.papp@echologic.org>
2011-02-25 17:59:30 +0100

    Added logs and delay.

--- lib/mailer_service/mailer_service.rb
+++ lib/mailer_service/mailer_service.rb
@@ -8,8 +8,8 @@ class MailerService
   def send_newsletter_mails(newsletter)
     User.find(:all, :conditions => {:newsletter_notification => 1}).each do |recipient|
       NewsletterMailer.deliver_newsletter_mail(recipient, newsletter)
-#      puts "Newsletter has been delivered to: " + recipient.email
-#      sleep 3
+      puts "Newsletter has been delivered to: " + recipient.email
+      sleep 2
     end
   end
 
@@ -17,6 +17,6 @@ class MailerService
   # Async calls #
   ###############
 
-#  handle_asynchronously :send_newsletter_mails
+  #handle_asynchronously :send_newsletter_mails
 
 end
