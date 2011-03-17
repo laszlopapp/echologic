3b844d768b51790dd9aa3878bff6d7499fe6aa1a
unknown <Tiago@cardoso@echologic.org>
2011-03-17 12:07:42 +0100

    introduced the actualized jQuery (1.5) to echo

--- app/helpers/connect_helper.rb
+++ app/helpers/connect_helper.rb
@@ -14,7 +14,7 @@ module ConnectHelper
         :id => 'close_details_container',
         :class => 'close_link',
         :onclick => "$('.profile').removeClass('active');
-                     $.scrollTo('top', 400, function(){
+                     $.scrollTo('body', 400, function(){
                        $('#profile_details_container').animate(toggleParams, 500);
                      });
                      return false;"
