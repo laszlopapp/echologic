417286086aceca857b7b7f314beca769251ef68a
Laszlo Papp <laszlo.papp@echologic.org>
2011-03-17 21:23:44 +0100

    Adjusted social network labels.

--- app/helpers/my_echo_helper.rb
+++ app/helpers/my_echo_helper.rb
@@ -1,16 +1,29 @@
 module MyEchoHelper
+
   def social_account_button(provider, token_url)
-    if current_user.has_provider? provider
-      render :partial => 'users/social_accounts/disconnect', :locals => {:provider => provider}
+    button = ''
+    connected = current_user.has_provider? provider
+    css_classes = "social_label #{provider.eql?('yahoo!') ? 'yahoo' : provider}#{connected ? ' connected' : ''}"
+    if connected
+      button << link_to('', connected.identifier,
+                        :target => "_blank",
+                        :class => css_classes)
+      button << render(:partial => 'users/social_accounts/disconnect',
+                       :locals => {:provider => provider})
     else
-      render :partial => 'users/social_accounts/connect', :locals => {:provider => provider, :token_url => token_url}
+      button << content_tag(:span, '',
+                            :class => css_classes)
+      button << render(:partial => 'users/social_accounts/connect',
+                       :locals => {:provider => provider,
+                                   :token_url => token_url})
     end
+    button
   end
-  
+
   def component_link(method, type, entry)
     label = I18n.t("application.general.#{method}")
     path = send("#{method.eql?('delete') ? 'user' : "#{method}_user"}_#{type}_path", entry.user, entry)
     css = "#{method.eql?('delete') ? 'ajax_delete' : 'ajax'} text_button #{method}_text_button"
-    link_to label, path, :class => css 
+    link_to label, path, :class => css
   end
 end
