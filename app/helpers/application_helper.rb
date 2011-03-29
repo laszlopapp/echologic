c4eae82f5f87e355c1be2f081997ba55c7fbe273
Laszlo Papp <laszlo.papp@echologic.org>
2011-03-29 22:54:23 +0200

    Refactored signup signin panel logic.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -116,6 +116,33 @@ module ApplicationHelper
   # POP-UP LOGIN HELPERS #
   ########################
 
+  def toggle_form_field(type)
+    toggle_type = type.eql?('signin') ? 'signup' : 'signin'
+    content = ''
+    content << content_tag(:span, "#{I18n.t("users.#{toggle_type}.member_tag")} >>>",
+                           :class => 'registry_label')
+    content << content_tag(:a, I18n.t("users.#{toggle_type}.label"),
+                           :class => 'toggle_button',
+                           :href => "##{toggle_type}")
+    content
+  end
+
+  def signing_header(type)
+    content = ''
+    content << content_tag(:span, content_tag(:h2, I18n.t("users.#{type}.via_echo")), :class => 'box_label')
+    content << I18n.t('application.general.or')
+    content << content_tag(:span, content_tag(:h2, I18n.t("users.#{type}.via_social")), :class => 'box_label')
+    content
+  end
+
+  def signin_social_link
+    janrain_login_widget signin_remote_url, :language => I18n.locale
+  end
+
+  def signup_social_link
+    janrain_login_widget signup_remote_url, :language => I18n.locale
+  end
+
   def redirect_token_url(opts= {})
     # first step: create the url to redirect everything in the end
     redirect_url = add_remote_url + '?' + (
@@ -136,7 +163,7 @@ module ApplicationHelper
       { :authenticity_token => form_authenticity_token }.collect { |n| "#{n[0]}=#{ u(n[1]) }" if n[1] }
     ).compact.join('&')
     <<-EOF
-    <iframe src='http://#{RPX_APP_NAME}/openid/embed?#{embed_params(url)}' scrolling='no' frameBorder='no'  
+    <iframe src='http://#{RPX_APP_NAME}/openid/embed?#{embed_params(url)}' scrolling='no' frameBorder='no'
     allowtransparency='true' style='width:400px;height:240px'></iframe>
     EOF
   end
