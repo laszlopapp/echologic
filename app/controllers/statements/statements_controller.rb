c1300ae07523229b41aac0353aad3ff081f9950b
unknown <Tiago@cardoso@echologic.org>
2011-03-31 19:14:14 +0200

    corrected the previous commit

--- app/controllers/statements/statements_controller.rb
+++ app/controllers/statements/statements_controller.rb
@@ -827,9 +827,10 @@ class StatementsController < ApplicationController
   # for more info about attributes, please check the StatementNode.search_discussions documentation
   #
   def search_discussions(opts = {})
-    languages = @language_preference_list
-    languages -= [@statement_node.original_language.code] if @statement_node and 
-                                                             @statement_node.original_language.code != I18n.locale.to_s
+    languages ||= @language_preference_list
+    original_language = @statement_node.original_language
+    languages -= [original_language.id] if @statement_node and 
+                                           original_language.code != I18n.locale.to_s
     StatementNode.search_discussions(opts.merge({:user => current_user,
                                                  :language_ids => languages,
                                                  :show_unpublished => current_user && current_user.has_role?(:editor)}))
