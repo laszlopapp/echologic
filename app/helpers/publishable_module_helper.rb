ed5a516ea4797c1f8750a58eea33a83e024d4590
Laszlo Papp <laszlo.papp@echologic.org>
2011-04-05 00:10:45 +0200

    Removed unused parameter 'search_terms' to generate new question link.

--- app/helpers/publishable_module_helper.rb
+++ app/helpers/publishable_module_helper.rb
@@ -15,7 +15,7 @@ module PublishableModuleHelper
   #
   # create question button above the discuss search results and on the left corner of my questions
   #
-  def create_question_link_for(search_terms=nil, origin=@origin)
+  def create_question_link_for(origin=@origin)
     link_to(new_question_url(:origin => origin, :bids => origin),
                              :id => 'create_question_link',
                              :class => 'add_new_button') do
