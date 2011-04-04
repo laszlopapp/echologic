fadedae096098b15f600be0837d802049ea8d366
Tiago Cardoso <tiago.cardoso@echologic.org>
2011-04-05 00:26:41 +0200

    added pt translations

--- db/seeds.rb
+++ db/seeds.rb
@@ -317,7 +317,7 @@ end
                               :value => value, :context=> "")
 end
 ["Thematic Partners","Thematische Partner","Partenaires Thématiques",
- "Parceiros Temáticas","Colaboradores Temáticas"].each_with_index do |value,index|
+ "Parceiros Temáticos","Colaboradores Temáticas"].each_with_index do |value,index|
   EnumValue.create_or_update!(:enum_key => EnumKey.find_by_code_and_type('thematic_partners','AboutCategory'),
                               :code => EnumKey.find_by_type_and_key('Language',index+1).code,
                               :value => value, :context=> "")
