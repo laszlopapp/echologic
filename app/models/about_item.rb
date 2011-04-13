df9285223c4c396a4327d6bb29f844673644ea9a
Laszlo Papp <laszlo.papp@echologic.org>
2011-04-13 17:20:54 +0200

    Using biggest size logos.

--- app/models/about_item.rb
+++ app/models/about_item.rb
@@ -4,7 +4,8 @@ class AboutItem < ActiveRecord::Base
   has_attached_file :photo,
                     :styles => {:small => "x45>",
                                 :big => "120x>",
-                                :bigger => "180x120>"},
+                                :bigger => "180x120>",
+                                :biggest => "300x200>"},
                     :default_url => "/images/default_:style_avatar.png"
   validates_attachment_size :photo,
                             :less_than => 5.megabytes
