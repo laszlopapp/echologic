125c5a61e2658d7c1459c7be9e10790558c04977
Laszlo Papp <laszlo.papp@echologic.org>
2011-04-17 23:45:46 +0200

    Only jRails is default. The rest is loaded via CDNs.

--- vendor/plugins/jrails/rails/init.rb
+++ vendor/plugins/jrails/rails/init.rb
@@ -10,6 +10,6 @@
 # for an example, see http://henrik.nyh.se/2008/05/rails-authenticity-token-with-jquery
 # ActionView::Helpers::PrototypeHelper::DISABLE_JQUERY_FORGERY_PROTECTION = true
 
-ActionView::Helpers::AssetTagHelper::JAVASCRIPT_DEFAULT_SOURCES = ['jquery','jquery-ui','jrails']
+ActionView::Helpers::AssetTagHelper::JAVASCRIPT_DEFAULT_SOURCES = ['jrails']
 ActionView::Helpers::AssetTagHelper::reset_javascript_include_default
 require 'jrails'
