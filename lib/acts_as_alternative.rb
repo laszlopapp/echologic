8467fadf70eba5c530225c7a8c19d114670adef9
Tiago Cardoso <tiago.cardoso@echologic.org>
2011-04-26 12:02:46 +0200

    implemented a first solution for the alternative statements pagination (still needs improvement, investigate further the has_many through with no table alias issue).

--- lib/acts_as_alternative.rb
+++ lib/acts_as_alternative.rb
@@ -18,11 +18,12 @@ module ActiveRecord
           
           belongs_to :hub, :class_name => 'CasHub', :foreign_key => 'parent_id' 
           
-          has_many :alternative_statements, 
-                   :finder_sql => 'SELECT DISTINCT * FROM statement_nodes s ' +
-                                  'LEFT OUTER JOIN statement_nodes hubs ON s.parent_id = hubs.id AND hubs.type = \'CasHub\' ' +
-                                  'WHERE s.parent_id is not null AND s.question_id is null AND s.parent_id = #{hub ? hub.id : -1} AND s.id != #{id}',
-                   :class_name => 'StatementNode'
+          #TODO: WHEN RAILS ALLOWS THIS TO WORK, PLEASE ACTIVATE THIS RELSHIP: currently fails because the has many
+          #      through joins the statement nodes table with itself (correct) without alias (false!). 
+          #      when all goes well, please update the paginated alternative statements function
+          #
+          #has_many :alternative_statements, :through => :hub, :source => :contrary_statements
+
           
           class_eval do
             class << self
@@ -37,6 +38,15 @@ module ActiveRecord
                 true
               end
             end
+            
+            def paginated_alternative_statements(page, per_page = nil)
+              per_page = alternative_statements.length if per_page.nil? or per_page < 0
+              per_page = 1 if per_page.to_i == 0
+              
+              (hub.nil? ? [] : hub.alternative_statements).paginate(self.class.base_class.default_scope.merge(:page => page, 
+                                                                                                              :per_page => per_page))
+            end
+            
           end # --- class_eval
           has_alternatives args
         end
