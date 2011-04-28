b1f112ba9ca30e8aa28c923a164a172e12a6a415
Tiago Cardoso <tiago.cardoso@echologic.org>
2011-04-28 12:17:24 +0200

    added js plugin to handle alternative panel related events

---
+++ public/javascripts/discuss/alternative.js
@@ -0,0 +1,58 @@
+(function($){
+
+  $.fn.alternative = function() {
+
+
+    // Merging settings with defaults
+    var settings = {
+      'highlight_color' : '#BFCFFE',
+      'normal_mode_color' : '#D4D3D3'
+    }
+		
+    return this.each(function() {
+      /* Creating echoable and binding its API */
+      var elem = $(this), alternativeApi = elem.data('alternativeApi');
+      if (alternativeApi) {
+        alternativeApi.reinitialize();
+      } else {
+        alternativeApi = new Alternative(elem);
+        elem.data('alternativeApi', alternativeApi);
+      }
+    });
+
+
+    /****************/
+    /* The echoable */
+    /****************/
+
+    function Alternative(statement) {
+      var alternative_panel = statement.find('.alternative_panel');
+      initialize();
+
+      /*
+       * Initializes an echoable statement in a form or in normal mode.
+       */
+      function initialize() {
+				
+      }
+
+
+
+      // Public API
+      $.extend(this,
+      {
+        reinitialize: function() {
+          initialize();
+        },
+				highlight: function() {
+					alternative_panel.animate({backgroundColor : settings['highlight_color']}, 300);
+				},
+				normal_mode: function() {
+					alternative_panel.animate({backgroundColor : settings['normal_mode_color']}, 300);
+				}
+      });
+    }
+
+  };
+
+})(jQuery);
