715c3576a2caed1d94387270679d23d8afce7a84
Laszlo Papp <laszlo.papp@echologic.org>
2011-04-29 19:24:28 +0200

    Adjusted mobile device detection script.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -314,9 +314,16 @@
     return false;
   }
 
-	
-	
+
+
 	function isMobileDevice() {
-		return navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) ||
-           navigator.userAgent.match(/iPhone/i)  || navigator.userAgent.match(/iPod/i);
+    var userAgent = navigator.userAgent.toLowerCase();
+		return userAgent.match(/android/i) ||
+           userAgent.match(/iphone/i)  ||
+           userAgent.match(/ipod/i) ||
+           userAgent.match(/ipad/i) ||
+           userAgent.match(/webos/i) ||
+           userAgent.match(/palm/i) ||
+           userAgent.match(/blackberry/i) ||
+           userAgent.match(/windows ce/i);
 	}
