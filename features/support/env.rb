66813f4217fe03a8a2545e8e9c0bcd755fda4226
Laszlo Papp <laszlo.papp@echologic.org>
2011-05-11 14:59:39 +0200

    Removed wrong dependancy to rsepc 2.0.0+

--- features/support/env.rb
+++ features/support/env.rb
@@ -12,7 +12,6 @@ require 'cucumber/rails/rspec'
 require 'cucumber/rails/world'
 require 'cucumber/rails/active_record'
 require 'cucumber/web/tableish'
-require 'cucumber/rspec/doubles'
 
 require 'webrat'
 require 'webrat/core/matchers'
