07dede9f2670ee981daa7eb9cf1db325644f971d
Laszlo Papp <laszlo.papp@echologic.org>
2011-06-23 00:21:36 +0200

    Adjusted event & subscription deletion (not yet working).

--- lib/activity_tracking_service/acts_as_subscribeable.rb
+++ lib/activity_tracking_service/acts_as_subscribeable.rb
@@ -17,8 +17,10 @@ module ActiveRecord
           args.compact! if args
           class_eval do
             has_many :subscriptions,
-                     :as => :subscribeable,
-                     :dependent => :destroy
+                     :as => :subscribeable
+
+            has_many :events,
+                     :as => :subscribeable
 
             has_many :subscribers,
                      :class_name => 'User',
@@ -26,12 +28,14 @@ module ActiveRecord
                                     'LEFT JOIN subscriptions s ON s.subscriber_id = u.id ' +
                                     'WHERE s.subscribeable_id = #{id}'
 
-            after_destroy :destroy_events
+            after_destroy :destroy_subscriptions_and_events
 
             #
-            # Destroys Events which would become orphaned after deleting this statement.
+            # Destroys Subscriptions and Events which would become orphaned after deleting this statement.
             #
-            def destroy_events
+            def destroy_subscriptions_and_events
+              self.subscriptions.destroy_all
+              self.events.destroy_all
               Event.destroy_all("event LIKE '%\"id\":#{self.id}%' AND event LIKE '%\"type\":#{self.class.name.downcase}%'")
             end
 
