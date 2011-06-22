63187acb8345dff0ea574ce80eddd8c8adf01d7d
Tiago Cardoso <tiago.cardoso@echologic.org>
2011-06-22 15:54:23 +0200

    added signature to user mail

--- app/models/mailers/profile_mailer.rb
+++ app/models/mailers/profile_mailer.rb
@@ -3,6 +3,7 @@ class ProfileMailer < ActionMailer::Base
   # Send a user email communication between two echo users
   def user_mail(sender, recipient, mail)
     subject       "[echo message] #{mail[:subject]}"
+    from          "noreply@echologic.org"
     recipients    [recipient.email]
     reply_to      [sender.email]
     sent_on       Time.now
