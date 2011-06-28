4e48261dc866646f313c6e940693936804bf1589
Tiago Cardoso <tiago.cardoso@echologic.org>
2011-06-28 15:30:29 +0200

    made follow up question inherit from question

--- app/models/statements/follow_up_question.rb
+++ app/models/statements/follow_up_question.rb
@@ -4,7 +4,7 @@ class FollowUpQuestion < Question
   belongs_to :question
 
   delegate :level, :ancestors, :topic_tags, :topic_tags=, :tags, :taggable?, :echoable?, :editorial_state_id,
-           :editorial_state_id=, :publishable?, :published, :locked_at, :supported?, :taggable?, :creator_id=,
+           :editorial_state_id=, :published, :locked_at, :supported?, :taggable?, :creator_id=,
            :creator_id, :creator, :author_support, :ancestors, :target_id, :to => :question
 
   validates_associated :question
@@ -29,10 +29,7 @@ class FollowUpQuestion < Question
   #################################################
 
   class << self
-    def children_types(opts={})
-      Question.children_types(opts)
-    end
-
+    
     def new_instance(attributes = nil)
       parent = attributes ? attributes.delete(:parent_id) : nil
       root = attributes.delete(:root_id)
@@ -49,14 +46,6 @@ class FollowUpQuestion < Question
     def is_top_statement?
       true
     end
-    
-    def self.publishable?
-      true
-    end
-    
-    def publishable?
-      true
-    end
 
     def children_joins
       " LEFT JOIN #{Statement.table_name} ON #{self.table_name}.statement_id = #{Statement.table_name}.id"
@@ -66,18 +55,5 @@ class FollowUpQuestion < Question
       sanitize_sql(["AND (#{Statement.table_name}.editorial_state_id = ? OR #{self.table_name}.creator_id = ?) ",
                         StatementState['published'].id, opts[:user] ? opts[:user].id : -1])
     end
-
-    #################################################
-    # string helpers (acts_as_echoable overwriting) #
-    #################################################
-
-    def support_tag
-      "recommend"
-    end
-
-    def unsupport_tag
-      "unrecommend"
-    end
   end
-
 end
