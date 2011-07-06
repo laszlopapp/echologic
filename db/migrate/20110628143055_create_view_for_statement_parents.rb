515cf597018f3af623350ed3f9df9f424ffe8f86
Laszlo Papp <laszlo.papp@echologic.org>
2011-07-06 23:36:58 +0200

    Formating.

--- db/migrate/20110628143055_create_view_for_statement_parents.rb
+++ db/migrate/20110628143055_create_view_for_statement_parents.rb
@@ -1,10 +1,12 @@
 class CreateViewForStatementParents < ActiveRecord::Migration
   def self.up
     create_view :statement_nodes_parents,
-     "SELECT id, type, parent_id, 
-        (SELECT IF (p.type='CasHub', parent_id, id) 
-         FROM statement_nodes p WHERE p.id = s.parent_id) 
-      AS parent_node_id 
+     "SELECT id,
+             type,
+             parent_id,
+             (SELECT IF (p.type='CasHub', parent_id, id)
+                FROM statement_nodes p WHERE p.id = s.parent_id)
+             AS parent_node_id
       FROM statement_nodes s;" do |t|
       t.column :id
       t.column :type
