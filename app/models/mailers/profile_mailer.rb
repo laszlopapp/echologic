9ab3707b7772709d2cd019a3367f018a95a71b89
Laszlo Papp <laszlo.papp@echologic.org>
2011-07-14 23:03:39 +0200

    Adjusted mail to user.

--- app/models/mailers/profile_mailer.rb
+++ app/models/mailers/profile_mailer.rb
@@ -1,13 +1,17 @@
 class ProfileMailer < ActionMailer::Base
-  
+
   # Send a user email communication between two echo users
   def user_mail(sender, recipient, mail)
     language = recipient.default_language
-    subject       I18n.t("mailers.user_mail.subject", :subject => mail[:subject])
+    subject       I18n.t("mailers.user_mail.subject",
+                         :subject => mail[:subject])
     from          "noreply@echologic.org"
     recipients    [recipient.email]
     reply_to      [sender.email]
     sent_on       Time.now
-    body          :sender => sender, :text => mail[:text], :language => language
+    body          :sender => sender,
+                  :recipient => recipient,
+                  :text => mail[:text],
+                  :language => language
   end
 end
