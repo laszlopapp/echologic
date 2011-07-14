1bf7da62d346bd828d1f62eacd9f55d553fe2fe9
Laszlo Papp <laszlo.papp@echologic.org>
2011-07-15 00:18:50 +0200

    Code opt.

--- public/javascripts/discuss/statement.js
+++ public/javascripts/discuss/statement.js
@@ -510,9 +510,8 @@
 				container.find('a.statement_link').bind("click", function() {
 					var childId = $(this).parent().attr('statement-id');
 					var key = generateKey($(this).parent().attr('class'));
-					var current_bids = $('#breadcrumbs').data('breadcrumbApi').getBreadcrumbStack(null);
+					var bids = $('#breadcrumbs').data('breadcrumbApi').getBreadcrumbStack(null);
 
-					var bids = current_bids;
           if(newLevel){ // necessary evil: erase all breadcrumbs after the parent of the clicked statement
             var or_index = bids.length == 0 ? 0 : $.inArray($.fragment().origin, bids);
             var level = or_index + (statement_index+1);
