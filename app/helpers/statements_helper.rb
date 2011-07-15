899bfbedab853ace4f15a9a883a4ef8b22a3a912
Tiago Cardoso <tiago.cardoso@echologic.org>
2011-07-15 22:42:13 +0200

    added alternative prefix text to new alternative text hint

--- app/helpers/statements_helper.rb
+++ app/helpers/statements_helper.rb
@@ -449,7 +449,10 @@ module StatementsHelper
   end
 
   def summary_hint_text(statement_node)
-    I18n.t("discuss.statements.text_hint.#{node_type(statement_node)}")
+    text = ""
+    text << I18n.t("discuss.statements.text_hint.alternative_prefix") if params[:hub].eql? 'alternative'
+    text << I18n.t("discuss.statements.text_hint.#{node_type(statement_node)}")
+    text
   end
 
   ##############
