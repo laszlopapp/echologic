416db183839696e53e8cbb4bd9c530aa9050cb68
Tiago Cardoso <tiago.cardoso@echologic.org>
2011-07-22 11:54:53 +0200

    added cs parameter to the incorporate form

--- app/helpers/incorporation_module_helper.rb
+++ app/helpers/incorporation_module_helper.rb
@@ -11,7 +11,7 @@ module IncorporationModuleHelper
         ((statement_node.times_passed == 0 and statement_document.author == current_user) or
          (statement_node.times_passed == 1 and statement_node.supported?(current_user))))
 
-      link_to(incorporate_statement_node_url(parent_node,:approved_ip => statement_node.id),
+      link_to(incorporate_statement_node_url(parent_node,:approved_ip => statement_node.id, :cs => params[:cs]),
              :id => 'incorporate_link',
              :class => 'ajax') do
          content_tag(:span, '',
