3d144913a26ad2be79ca8d2add8858d7c1c20fe8
Tiago Cardoso <tiago.cardoso@echologic.org>
2011-07-28 09:49:45 +0200

    corrected the linking parent id bug

--- public/javascripts/discuss/statement_form.js
+++ public/javascripts/discuss/statement_form.js
@@ -53,7 +53,15 @@
         if (form.hasClass('new')) {
 					language_combo = form.find('.statement_language select');
 					statementLinked = form.find('input#statement_node_statement_id');
-					statementParentId = form.find('input#statement_node_parent_id');
+					
+					var parentForLinking = form.prev();
+					if(parentForLinking.length > 0) {
+						statementParentId = getStatementId(parentForLinking.attr('id'));
+					} else {
+					  statementParentId = form.find('input#statement_node_parent_id').val();	
+					}
+					
+					
           loadDefaultText();
           initFormCancelButton();
 					initLinking();
@@ -179,7 +187,6 @@
 
 				linkButton.removeAttr('linking_on').removeAttr('linking_off');
 
-
 				// initialize the autocompletion plugin
 				title.autocompletes('../../statements/auto_complete_for_statement_title', {
           minChars: 100,
@@ -187,7 +194,7 @@
           multipleSeparator: "",
           extraParams: {
             code: function(){ return chosenLanguage.val(); },
-            parent_id: function() {return statementParentId.val(); },
+            parent_id: function() {return statementParentId; },
             type: type
           }
         });
@@ -236,7 +243,7 @@
 				var path = '../../statement/' + nodeId + '/link_statement_node/' + type;
         path = $.queryString(path, {
           "code" : chosenLanguage.val(),
-					"parent_id": statementParentId.val()
+					"parent_id": statementParentId
         });
         $.getJSON(path, function(data) {
 					if (data['error']) {
