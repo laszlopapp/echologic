8d9b17fc38a13fc2072ecbfa598188536940b805
Tiago Cardoso <tiago.cardoso@echologic.org>
2011-08-01 14:03:04 +0200

    disabled the navigation buttons for the discuss alternatives question

--- app/helpers/statements_helper.rb
+++ app/helpers/statements_helper.rb
@@ -580,7 +580,7 @@ module StatementsHelper
   #
   def navigation_buttons(statement_node, type, opts={})
     buttons = ''
-    if statement_node and statement_node.new_record?
+    if statement_node and (statement_node.new_record? or opts[:origin] and opts[:origin][0,2].eql?('dq'))
       %w(prev next).each{|button| buttons << statement_tag(button.to_sym, type, true)}
       buttons << content_tag(:span,
                              I18n.t("discuss.statements.sibling_labels.#{type.classify.constantize.name_for_siblings}"),
